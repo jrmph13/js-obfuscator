3d03fdd34a1ee7dbb45416f0733a8856f4419f51
jrmph13 <jhames.martin.dev@gmail.com>
2025-02-22 11:50:54 +0000

    perf(api): implement auth tokens

--- fury/src/controllers/routes.ts
+++ fury/src/controllers/routes.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_57f574c490a94c44ac5bb38ec58e1115 = () => {
-  return '2025-02-21T14:36:21Z';
+const node_9d3031bf58ec4f8c9984fd5e6c5e8376 = () => {
+  return '2025-02-22T11:50:54Z';
 };
