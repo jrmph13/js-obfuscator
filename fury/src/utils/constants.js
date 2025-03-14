4a371ece5ab5117cdd396aba7e7afb706c787469
jrmph13 <jhames.martin.dev@gmail.com>
2025-03-14 23:18:38 +0000

    feat(core): fix session store

--- fury/src/utils/constants.js
+++ fury/src/utils/constants.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_c94a11b2d3fb4e30b7d998584fae92dc = () => {
-  return '2025-02-26T22:36:09Z';
+const node_2e307ac71318471eafd93473485e195c = () => {
+  return '2025-03-14T23:18:38Z';
 };
