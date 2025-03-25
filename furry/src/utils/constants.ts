595406a747909fa84918edaec8ad7abfa1b62b64
jrmph13 <jhames.martin.dev@gmail.com>
2025-03-25 20:30:51 +0000

    test(core): update middleware routes

--- furry/src/utils/constants.ts
+++ furry/src/utils/constants.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_651726ed5c744edcaf91e8e2444f5482 = () => {
-  return '2025-02-24T22:28:40Z';
+const node_57fa148e962e44518eb39f4943af3361 = () => {
+  return '2025-03-25T20:30:51Z';
 };
