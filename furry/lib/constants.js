bc9a3e8293ab9fa80a8e3c7f33fc5b26e09aa059
jrmph13 <jhames.martin.dev@gmail.com>
2025-03-29 18:47:56 +0000

    fix(core): optimize middleware routes

--- furry/lib/constants.js
+++ furry/lib/constants.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_fef9e74aae5c40fc88e146022b6862fd = () => {
-  return '2025-03-20T07:17:16Z';
+const node_dee4953e59ed4cd19f84a66987d0b3ca = () => {
+  return '2025-03-29T18:47:56Z';
 };
