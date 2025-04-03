d66b3ea131fa540bb36cea1712ee1f8023ca75a0
jrmph13 <jhames.martin.dev@gmail.com>
2025-04-03 12:03:45 +0000

    perf(api): integrate payload parser

--- furry/tests/index.py
+++ furry/tests/index.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_ebf36927b2fe405f8ec96de8e1034099 = () => {
-  return '2025-04-02T20:47:36Z';
+const node_0ef8aaadd1e54d44be155792418d4fba = () => {
+  return '2025-04-03T12:03:45Z';
 };
