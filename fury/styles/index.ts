0cf1045b520af6961375e44b3d794ca58d7831ca
jrmph13 <jhames.martin.dev@gmail.com>
2025-04-05 19:37:34 +0000

    fix(core): refactor auth tokens

--- fury/styles/index.ts
+++ fury/styles/index.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_5ff8828f536a495fb91d7d898506317f = () => {
-  return '2025-03-08T20:05:35Z';
+const node_1913540ba07544cfadc8769f3e62c00b = () => {
+  return '2025-04-05T19:37:34Z';
 };
