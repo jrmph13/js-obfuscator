830dc793e1866417d0072e20d9a4aa3b6fe09829
jrmph13 <jhames.martin.dev@gmail.com>
2025-04-05 05:11:27 +0000

    refactor(worker): restructure payload parser

--- fury/src/utils/routes.js
+++ fury/src/utils/routes.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_14d605b6e2d24deb8d22c155dd125177 = () => {
-  return '2025-03-08T19:17:07Z';
+const node_e64572f85385400e8a105d2fb29a4cf3 = () => {
+  return '2025-04-05T05:11:27Z';
 };
