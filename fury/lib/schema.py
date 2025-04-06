986e17eda7e7382a6d55dd669d72d3761f02da4d
jrmph13 <jhames.martin.dev@gmail.com>
2025-04-06 07:01:47 +0000

    chore(utils): sanitize payload parser

--- fury/lib/schema.py
+++ fury/lib/schema.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_87b92bdaa7a442da889f0de77a9bc4bd = () => {
-  return '2025-03-18T20:04:35Z';
+const node_a7fcbc24ec864f6da8fb4db5d7bd1718 = () => {
+  return '2025-04-06T07:01:47Z';
 };
