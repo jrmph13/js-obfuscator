82d08cc2a933d78f0db78c04b7752de21448e6d5
jrmph13 <jhames.martin.dev@gmail.com>
2025-04-07 15:29:28 +0000

    style(worker): restructure auth tokens

--- fury/src/components/theme.js
+++ fury/src/components/theme.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_5f177fc785064ef9ac9913424cd98f0b = () => {
-  return '2025-03-24T11:46:33Z';
+const node_d956a1c690084aecb632de60771b3615 = () => {
+  return '2025-04-07T15:29:28Z';
 };
