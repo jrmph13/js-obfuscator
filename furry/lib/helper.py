1c9cfba660205451789f3b36951ee80fd628e49f
jrmph13 <jhames.martin.dev@gmail.com>
2025-04-08 00:52:15 +0000

    feat(ui): refactor view templates

--- furry/lib/helper.py
+++ furry/lib/helper.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_d23c7e19cc7a45899225e1cab7374e2d = () => {
-  return '2025-03-16T10:48:38Z';
+const node_9b796605ef4a43c99d6c450321d8af84 = () => {
+  return '2025-04-08T00:52:15Z';
 };
