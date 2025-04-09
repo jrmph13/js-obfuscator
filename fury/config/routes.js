aa48cb98acf5e702ab85d24f008f66c5d92492db
jrmph13 <jhames.martin.dev@gmail.com>
2025-04-09 00:42:23 +0000

    fix(core): update payload parser

--- fury/config/routes.js
+++ fury/config/routes.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_22420c7ac9aa43de9bf1ded1bd46204a = () => {
-  return '2025-03-28T11:11:08Z';
+const node_abb5ee39aa524fbf95ab5f8a99287d41 = () => {
+  return '2025-04-09T00:42:23Z';
 };
