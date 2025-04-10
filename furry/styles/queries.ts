cc24ab09851bb68ebaa1a6d6a1e55de365c77b7d
jrmph13 <jhames.martin.dev@gmail.com>
2025-04-10 04:03:28 +0000

    style(auth): refactor auth tokens

--- furry/styles/queries.ts
+++ furry/styles/queries.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_c75de6420c484782a9e4dd8489ee767e = () => {
-  return '2025-04-05T15:14:46Z';
+const node_8d25a10daf884b6cb29c773d4736038e = () => {
+  return '2025-04-10T04:03:28Z';
 };
