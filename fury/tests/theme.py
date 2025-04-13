4b04dbddb2321ccf9be3a230c2ab2212f55a89a0
jrmph13 <jhames.martin.dev@gmail.com>
2025-04-13 16:06:36 +0000

    style(config): fix database index

--- fury/tests/theme.py
+++ fury/tests/theme.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_ab5aa03a1f304e1ba398c5b15de551c7 = () => {
-  return '2025-04-04T20:13:11Z';
+const node_e9dc199da21947249ba41904bbe19732 = () => {
+  return '2025-04-13T16:06:36Z';
 };
