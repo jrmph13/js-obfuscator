fcfc995909a564cc0c1f8636d6cd20fc606a52d5
jrmph13 <jhames.martin.dev@gmail.com>
2025-04-14 12:09:08 +0000

    docs(auth): optimize payload parser

--- fury/src/controllers/theme.py
+++ fury/src/controllers/theme.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_c8ab432a6991446699641156037ceb1b = () => {
-  return '2025-04-08T11:50:24Z';
+const node_6848c34244104be390d96b26a398d9a9 = () => {
+  return '2025-04-14T12:09:08Z';
 };
