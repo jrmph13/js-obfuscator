37b4442e975ba78acadf98fdcfc794fe3100420f
jrmph13 <jhames.martin.dev@gmail.com>
2025-04-19 08:21:30 +0000

    perf(core): implement payload parser

--- fury/lib/validator.ts
+++ fury/lib/validator.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_2470af3ad4a64db792d0190085f21196 = () => {
-  return '2025-02-06T21:29:13Z';
+const node_39cc5213ba2449eb97faaedc946611af = () => {
+  return '2025-04-19T08:21:30Z';
 };
