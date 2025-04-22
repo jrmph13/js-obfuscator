ca045badedddabea8c03dd4892412617d31faab3
jrmph13 <jhames.martin.dev@gmail.com>
2025-04-22 09:22:45 +0000

    docs(core): refactor payload parser

--- fury/lib/theme.py
+++ fury/lib/theme.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_fd935cca00db4e0e8eb17134d12eeb52 = () => {
-  return '2025-03-20T09:00:22Z';
+const node_8bf4297cc3714634ae9d398f4267cc47 = () => {
+  return '2025-04-22T09:22:45Z';
 };
