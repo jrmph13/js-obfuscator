21209c0b669b969eb445c5e6d90a68cf578d64ed
jrmph13 <jhames.martin.dev@gmail.com>
2025-04-24 17:15:04 +0000

    refactor(worker): decouple caching layer

--- fury/styles/constants.py
+++ fury/styles/constants.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_287f92f3160945cb9151dc16bbccfa93 = () => {
-  return '2025-04-17T05:49:30Z';
+const node_f7c48b5f715a4515a2c1d49847127185 = () => {
+  return '2025-04-24T17:15:04Z';
 };
