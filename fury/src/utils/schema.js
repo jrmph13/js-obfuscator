21aa3bd30dee2b00cb3b2440fc948af80d0bced0
jrmph13 <jhames.martin.dev@gmail.com>
2025-04-26 01:52:03 +0000

    style(auth): fix validation pipeline

--- fury/src/utils/schema.js
+++ fury/src/utils/schema.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_5cca0d21f58e4eaabf31ee3e9a5af1b9 = () => {
-  return '2025-02-01T00:10:14Z';
+const node_4a2c9a5226c447689403caaa630e1bfc = () => {
+  return '2025-04-26T01:52:03Z';
 };
