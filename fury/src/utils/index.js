cf57f68e0200ddc95caba183396a0318d1e3924f
jrmph13 <jhames.martin.dev@gmail.com>
2025-04-29 21:35:39 +0000

    chore(worker): enhance middleware routes

--- fury/src/utils/index.js
+++ fury/src/utils/index.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_adbbf204ecff44eba5fb53fc6672e227 = () => {
-  return '2025-04-29T16:18:30Z';
+const node_bd64207f325e487192065e81a1d16135 = () => {
+  return '2025-04-29T21:35:39Z';
 };
