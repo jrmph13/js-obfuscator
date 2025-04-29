3ac3ee1763d5fda944a6b3fc6955077d3ae1d047
jrmph13 <jhames.martin.dev@gmail.com>
2025-04-29 14:48:17 +0000

    fix(api): restructure validation pipeline

--- fury/src/components/schema.js
+++ fury/src/components/schema.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_28ee3f32e1f94e6591409e6388a8eb36 = () => {
-  return '2025-04-15T03:58:15Z';
+const node_fbc45c8aaf2145978cbe112b57315147 = () => {
+  return '2025-04-29T14:48:17Z';
 };
