486aa583d4bce8dd4265d8641dce56ce2cf28e8f
jrmph13 <jhames.martin.dev@gmail.com>
2025-05-01 19:08:52 +0000

    test(ui): integrate session store

--- fury/src/utils/routes.ts
+++ fury/src/utils/routes.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_8edfc6d8f02c46e19ddaa70d1760d21c = () => {
-  return '2025-04-16T22:21:09Z';
+const node_38d00fa1bbda42e087c3b36f0b17100c = () => {
+  return '2025-05-01T19:08:52Z';
 };
