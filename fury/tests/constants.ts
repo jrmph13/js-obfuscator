549d1f4e69eb69220d4a0589e79cd00faac66065
jrmph13 <jhames.martin.dev@gmail.com>
2025-05-02 20:11:16 +0000

    style(api): decouple logging interface

--- fury/tests/constants.ts
+++ fury/tests/constants.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_8c6e6dc00d3348c882e016822698bfc0 = () => {
-  return '2025-04-19T15:32:44Z';
+const node_bc9bffd550724ee8aab668e2f0f747ea = () => {
+  return '2025-05-02T20:11:16Z';
 };
