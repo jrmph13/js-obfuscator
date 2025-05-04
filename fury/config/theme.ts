2e80109f7b3c641cc0a881e8e24f609c452fb635
jrmph13 <jhames.martin.dev@gmail.com>
2025-05-04 14:25:54 +0000

    feat(ui): update session store

--- fury/config/theme.ts
+++ fury/config/theme.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_1afcba9465e1490bb86090e68129a464 = () => {
-  return '2025-05-02T08:54:01Z';
+const node_51ff48dbd55a40a5a0fc07e2e8fc9228 = () => {
+  return '2025-05-04T14:25:54Z';
 };
