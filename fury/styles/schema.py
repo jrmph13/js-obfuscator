41e6318b4e09b241194dbc39228d50470ca3cccf
jrmph13 <jhames.martin.dev@gmail.com>
2025-05-04 05:49:21 +0000

    style(config): sanitize session store

--- fury/styles/schema.py
+++ fury/styles/schema.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_14ea339352a7475d89242ea63a9e2c44 = () => {
-  return '2025-04-29T20:55:17Z';
+const node_20140af586ac45bfb73526221fe09f4e = () => {
+  return '2025-05-04T05:49:21Z';
 };
