5066c2ca2803b2f393233ec870b71a12c1803010
jrmph13 <jhames.martin.dev@gmail.com>
2025-05-05 01:09:36 +0000

    chore(worker): decouple caching layer

--- furry/styles/queries.ts
+++ furry/styles/queries.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_8d25a10daf884b6cb29c773d4736038e = () => {
-  return '2025-04-10T04:03:28Z';
+const node_ea73db3c2e2c47e78381f2fb247bf137 = () => {
+  return '2025-05-05T01:09:36Z';
 };
