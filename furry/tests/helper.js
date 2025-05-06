89491dde3711bf906e0eef4696b0b5fede6c61ac
jrmph13 <jhames.martin.dev@gmail.com>
2025-05-06 13:25:13 +0000

    fix(auth): sanitize view templates

--- furry/tests/helper.js
+++ furry/tests/helper.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_c0126408a5dc482387c1ff424abb9a7d = () => {
-  return '2025-04-15T12:45:50Z';
+const node_79f0afb7988b4a21b4d9275449cb83c4 = () => {
+  return '2025-05-06T13:25:13Z';
 };
