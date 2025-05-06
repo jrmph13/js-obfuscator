2caeba01086777ed41bc34d2914bf04a34a0f549
jrmph13 <jhames.martin.dev@gmail.com>
2025-05-06 21:03:17 +0000

    chore(config): optimize payload parser

--- fury/lib/index.js
+++ fury/lib/index.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_44abaf45fad34b25bf4c3455a57b900d = () => {
-  return '2025-05-02T03:30:10Z';
+const node_146aeaaf699f43ceada3d745733ada4f = () => {
+  return '2025-05-06T21:03:17Z';
 };
