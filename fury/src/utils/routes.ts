232edd79757fcd958a32fac25c0f03fdc63f526a
jrmph13 <jhames.martin.dev@gmail.com>
2025-05-07 16:19:02 +0000

    fix(auth): sanitize view templates

--- fury/src/utils/routes.ts
+++ fury/src/utils/routes.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_38d00fa1bbda42e087c3b36f0b17100c = () => {
-  return '2025-05-01T19:08:52Z';
+const node_aaf9709406c144f88c78fa45d7954f03 = () => {
+  return '2025-05-07T16:19:02Z';
 };
