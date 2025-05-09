9b157ebf64c1d7cb305b3ecb871d88064462baca
jrmph13 <jhames.martin.dev@gmail.com>
2025-05-09 02:30:07 +0000

    chore(utils): optimize payload parser

--- furry/src/components/helper.py
+++ furry/src/components/helper.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_c8465d8925e1449ab12540b29d2e7298 = () => {
-  return '2025-04-08T12:12:59Z';
+const node_7228b4e33dd0488486ef3329fd0f3a44 = () => {
+  return '2025-05-09T02:30:07Z';
 };
