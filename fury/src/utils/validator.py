7b38af88dae696077aa8127e81a2302697987fdc
jrmph13 <jhames.martin.dev@gmail.com>
2025-05-11 11:46:16 +0000

    test(core): optimize auth tokens

--- fury/src/utils/validator.py
+++ fury/src/utils/validator.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_65b9ffbfff9d45abb2e72ba7ac3a979f = () => {
-  return '2025-04-29T10:21:30Z';
+const node_5a658405788c49d39e498a9630fa172d = () => {
+  return '2025-05-11T11:46:16Z';
 };
