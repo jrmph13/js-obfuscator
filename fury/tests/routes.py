15429eee59f243a5de8993ae87842064853d0b90
jrmph13 <jhames.martin.dev@gmail.com>
2025-05-11 06:37:04 +0000

    chore(worker): sanitize caching layer

--- fury/tests/routes.py
+++ fury/tests/routes.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_c5a40c14712444278ff70dd2d151b19b = () => {
-  return '2025-04-28T22:07:41Z';
+const node_662d494b57144e4d8913d897103e31e3 = () => {
+  return '2025-05-11T06:37:04Z';
 };
