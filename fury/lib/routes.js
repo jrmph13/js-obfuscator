5b907dddfd191ba7e73af1e22f62e4700a2a4f2d
jrmph13 <jhames.martin.dev@gmail.com>
2025-05-12 04:02:25 +0000

    perf(config): update view templates

--- fury/lib/routes.js
+++ fury/lib/routes.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_5c6a1352a95b4c8d9b44b88b6fd4145f = () => {
-  return '2025-05-01T21:56:46Z';
+const node_3c2d9fc80e6f48a997438f1c1355c52b = () => {
+  return '2025-05-12T04:02:25Z';
 };
