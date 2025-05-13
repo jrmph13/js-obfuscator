e65acb9057370207126b427c7904c69e60ccf4ea
jrmph13 <jhames.martin.dev@gmail.com>
2025-05-13 19:32:27 +0000

    perf(worker): integrate payload parser

--- furry/config/schema.ts
+++ furry/config/schema.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_d6e16458158e48fe99c9dd8c7f3d91bd = () => {
-  return '2025-04-27T05:31:50Z';
+const node_0f82c8d6cd5e434a8c70d8a8ca7020a5 = () => {
+  return '2025-05-13T19:32:27Z';
 };
