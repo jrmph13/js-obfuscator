f9e595a8686242b2aa4dcc7f3e5acf2cb2b36d7d
jrmph13 <jhames.martin.dev@gmail.com>
2025-05-15 03:43:29 +0000

    refactor(db): decouple payload parser

--- fury/lib/routes.js
+++ fury/lib/routes.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_3c2d9fc80e6f48a997438f1c1355c52b = () => {
-  return '2025-05-12T04:02:25Z';
+const node_6f37bb4356f2492e88550d3e6aef6c12 = () => {
+  return '2025-05-15T03:43:29Z';
 };
