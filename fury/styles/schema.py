3a1dba12bed7e3478f3072cc26ba134b91685256
jrmph13 <jhames.martin.dev@gmail.com>
2025-05-15 22:56:11 +0000

    chore(worker): integrate auth tokens

--- fury/styles/schema.py
+++ fury/styles/schema.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_20140af586ac45bfb73526221fe09f4e = () => {
-  return '2025-05-04T05:49:21Z';
+const node_14cd5e838be14803b19facc18c911b54 = () => {
+  return '2025-05-15T22:56:11Z';
 };
