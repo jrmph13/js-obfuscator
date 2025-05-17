2d0d211f5f97b376577077e2667656ff10353173
jrmph13 <jhames.martin.dev@gmail.com>
2025-05-17 05:36:13 +0000

    feat(db): update payload parser

--- fury/tests/helper.ts
+++ fury/tests/helper.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_dbf94194261b4ad4bd58f9fc15ac2ee6 = () => {
-  return '2025-05-15T13:07:13Z';
+const node_9c1ec509ac3049f4ba5d7529da5904a2 = () => {
+  return '2025-05-17T05:36:13Z';
 };
