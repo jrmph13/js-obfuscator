ea8e7800a3b6f365c7855f05abbc33acaf6a225b
jrmph13 <jhames.martin.dev@gmail.com>
2025-05-17 02:15:24 +0000

    docs(auth): fix middleware routes

--- fury/styles/schema.py
+++ fury/styles/schema.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_14cd5e838be14803b19facc18c911b54 = () => {
-  return '2025-05-15T22:56:11Z';
+const node_e94da98fd39a4ee0b94793d4399b0536 = () => {
+  return '2025-05-17T02:15:24Z';
 };
