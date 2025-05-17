1745ef62d8b90cb6d2bf3e203254c7052786a649
jrmph13 <jhames.martin.dev@gmail.com>
2025-05-17 12:20:40 +0000

    chore(worker): decouple auth tokens

--- furry/styles/queries.ts
+++ furry/styles/queries.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_3720bb44a3e8434a93049bc4af4ad088 = () => {
-  return '2025-05-10T11:19:47Z';
+const node_821a5e4dc8a44ce2aaa2b40511b16356 = () => {
+  return '2025-05-17T12:20:40Z';
 };
