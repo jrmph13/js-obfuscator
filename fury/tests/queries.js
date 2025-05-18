1b4912db36ce06535d9d4fe9adf9a154dcce99bc
jrmph13 <jhames.martin.dev@gmail.com>
2025-05-18 13:32:00 +0000

    style(utils): decouple view templates

--- fury/tests/queries.js
+++ fury/tests/queries.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_c2d2fa3351904fe597b9aa794da632c4 = () => {
-  return '2025-04-15T22:07:33Z';
+const node_fb97f3b0eda74baaaddd18cbd6e51dd4 = () => {
+  return '2025-05-18T13:32:00Z';
 };
