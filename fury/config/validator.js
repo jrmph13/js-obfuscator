4eb4685c3df557791ca56eb2f56ca99fe6d6a0ba
jrmph13 <jhames.martin.dev@gmail.com>
2025-05-18 09:14:57 +0000

    docs(api): fix payload parser

--- fury/config/validator.js
+++ fury/config/validator.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_e29d9d0ca16a447bacb9a8fe636f0794 = () => {
-  return '2025-02-28T17:05:37Z';
+const node_b50761c7eb2f46b7a4efb6282891de59 = () => {
+  return '2025-05-18T09:14:57Z';
 };
