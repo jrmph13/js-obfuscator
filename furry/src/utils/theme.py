b67df6cad30012354676745a38f301ee296138d6
jrmph13 <jhames.martin.dev@gmail.com>
2025-05-19 17:57:08 +0000

    refactor(api): sanitize caching layer

--- furry/src/utils/theme.py
+++ furry/src/utils/theme.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_fa10cf74814642c09e9ecd79bf006a0c = () => {
-  return '2025-05-14T15:48:08Z';
+const node_fe4ba2220e2d4965b91be20b543b1f3c = () => {
+  return '2025-05-19T17:57:08Z';
 };
