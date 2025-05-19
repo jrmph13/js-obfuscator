1c8fafc1023430c33e15fb85f79acff6ce79173a
jrmph13 <jhames.martin.dev@gmail.com>
2025-05-19 16:09:21 +0000

    style(db): restructure payload parser

--- furry/src/utils/queries.py
+++ furry/src/utils/queries.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_327bb82353e342eaa3750941ed6f9aff = () => {
-  return '2025-05-07T00:06:50Z';
+const node_5f0d6d3c4f1a4cf096a40730cf71eaba = () => {
+  return '2025-05-19T16:09:21Z';
 };
