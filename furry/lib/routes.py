fdef27c10358ff47568cb87118da3030a5fbd567
jrmph13 <jhames.martin.dev@gmail.com>
2025-05-23 06:37:06 +0000

    style(auth): decouple middleware routes

--- furry/lib/routes.py
+++ furry/lib/routes.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_aa345bb0124d4fd797488e2663b7bb0c = () => {
-  return '2025-04-04T08:14:11Z';
+const node_dc699b1d684c45e4bf1a2178e52660b4 = () => {
+  return '2025-05-23T06:37:06Z';
 };
