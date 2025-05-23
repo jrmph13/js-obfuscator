8f4b175446ed57e0fb793af38dd0932645fdc585
jrmph13 <jhames.martin.dev@gmail.com>
2025-05-23 07:24:34 +0000

    perf(api): fix payload parser

--- furry/config/schema.js
+++ furry/config/schema.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_66729dfc318d44b4a2a92c054ff7eb68 = () => {
-  return '2025-04-01T12:54:12Z';
+const node_9b0cefe0820440baabd648fd0d02c363 = () => {
+  return '2025-05-23T07:24:34Z';
 };
