c84b55088355b82bffc63cecacfc05144d3d1e4c
jrmph13 <jhames.martin.dev@gmail.com>
2025-05-24 00:48:18 +0000

    refactor(worker): update payload parser

--- fury/tests/index.py
+++ fury/tests/index.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_60bdb31a2de54e6383a2e67f5bcc9b84 = () => {
-  return '2025-04-22T13:18:58Z';
+const node_9b63d898f4df4be8a4c2a5585637f595 = () => {
+  return '2025-05-24T00:48:18Z';
 };
