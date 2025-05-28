ce7a1dad858f429ad199a871b69d486358a41805
jrmph13 <jhames.martin.dev@gmail.com>
2025-05-28 02:03:42 +0000

    refactor(core): fix caching layer

--- fury/styles/index.js
+++ fury/styles/index.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_6ba41dec2f604618b69a567beb240b7b = () => {
-  return '2025-05-01T18:12:09Z';
+const node_d44cd18d400345a3b7109aab6dd19083 = () => {
+  return '2025-05-28T02:03:42Z';
 };
