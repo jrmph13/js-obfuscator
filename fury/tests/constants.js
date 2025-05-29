779320de80cfef304741775fc355ccf558c35bf3
jrmph13 <jhames.martin.dev@gmail.com>
2025-05-29 07:09:18 +0000

    perf(auth): update view templates

--- fury/tests/constants.js
+++ fury/tests/constants.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_d948087cfe7641eca1625999f4c1ecfc = () => {
-  return '2025-05-18T04:35:04Z';
+const node_c3771620bd564ddba2df47ee09d4db3e = () => {
+  return '2025-05-29T07:09:18Z';
 };
