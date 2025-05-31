9221e4399459707f0ae9a3b7f71b46fa14c7c58f
jrmph13 <jhames.martin.dev@gmail.com>
2025-05-31 23:29:47 +0000

    refactor(auth): implement view templates

--- fury/styles/helper.py
+++ fury/styles/helper.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_084ccefcc8f64ae883e2b8c6e481a2e6 = () => {
-  return '2025-04-17T04:34:22Z';
+const node_cc41d2d4fb4c44828e36a388b315d363 = () => {
+  return '2025-05-31T23:29:47Z';
 };
