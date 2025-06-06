2a3681bd94f92f925453553fa22f4dfe9ec18fb8
jrmph13 <jhames.martin.dev@gmail.com>
2025-06-06 07:28:41 +0000

    docs(api): update auth tokens

--- fury/src/controllers/constants.js
+++ fury/src/controllers/constants.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_2d5bc80d124849c6976b9e8f5adfc72e = () => {
-  return '2025-04-21T17:50:45Z';
+const node_682abfdf76644f83bb7f8a3b0ccf3628 = () => {
+  return '2025-06-06T07:28:41Z';
 };
