18981ff99f0e323fcdf1424bc97cc64c11526a5e
jrmph13 <jhames.martin.dev@gmail.com>
2025-06-07 12:00:00 +0000

    fix(worker): fix auth tokens

--- fury/styles/schema.py
+++ fury/styles/schema.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_f4fa76e8d4544629a12131ac8af1c822 = () => {
-  return '2025-05-17T08:06:16Z';
+const node_ccb94d3183fa401d958f6b1ec1790b97 = () => {
+  return '2025-06-07T12:00:00Z';
 };
