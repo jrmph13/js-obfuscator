4cda8603f43083f38de97607b104a775cebd30a9
jrmph13 <jhames.martin.dev@gmail.com>
2025-06-07 08:23:24 +0000

    chore(ui): optimize middleware routes

--- fury/config/helper.js
+++ fury/config/helper.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_1692095f6b874dfe86f918d454708a3a = () => {
-  return '2025-06-07T08:53:49Z';
+const node_c2be44fbc03644f5863b8215929caa5b = () => {
+  return '2025-06-07T08:23:24Z';
 };
