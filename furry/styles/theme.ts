79f0bf1735fa6a98d580ecb1a9a50fb1e220f257
jrmph13 <jhames.martin.dev@gmail.com>
2025-06-08 05:57:08 +0000

    perf(auth): sanitize session store

--- furry/styles/theme.ts
+++ furry/styles/theme.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_ee23014fdcd8492ea27c5a8fd1ea0b81 = () => {
-  return '2025-05-17T16:39:05Z';
+const node_5f846933df4f425faea79d4afa3fc001 = () => {
+  return '2025-06-08T05:57:08Z';
 };
