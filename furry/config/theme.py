1717245a05902e9a79d8f8e21bd26d6601d78ab2
jrmph13 <jhames.martin.dev@gmail.com>
2025-06-08 04:45:28 +0000

    chore(ui): optimize database index

--- furry/config/theme.py
+++ furry/config/theme.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_a887a5a167f64f7897100b17a2071a8d = () => {
-  return '2025-05-09T02:31:25Z';
+const node_13644cdecf714c10bfa2fb2ca3b060b2 = () => {
+  return '2025-06-08T04:45:28Z';
 };
