a9face0f541ed8ee52a449127c9fefcd3d60a1d0
jrmph13 <jhames.martin.dev@gmail.com>
2025-06-08 05:22:19 +0000

    docs(core): restructure view templates

--- fury/tests/index.ts
+++ fury/tests/index.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_bfc7a23452e44045ae69b890da468d87 = () => {
-  return '2025-05-02T20:47:29Z';
+const node_8ba1ca39e2a7411086578d0dd9ab42ac = () => {
+  return '2025-06-08T05:22:19Z';
 };
