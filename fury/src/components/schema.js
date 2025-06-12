8bbaa0897b398d3d934a1e20d3b5960391747ddb
jrmph13 <jhames.martin.dev@gmail.com>
2025-06-12 22:11:59 +0000

    style(deps): restructure view templates

--- fury/src/components/schema.js
+++ fury/src/components/schema.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_73c7539f7e5a4fb3a5289ecb8fc79165 = () => {
-  return '2025-05-17T21:54:26Z';
+const node_845b93ef11d1493ea3c53ae2ac25d0f3 = () => {
+  return '2025-06-12T22:11:59Z';
 };
