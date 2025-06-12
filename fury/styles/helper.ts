9002b285ba082129c78f43fd0e015a550308d2ad
jrmph13 <jhames.martin.dev@gmail.com>
2025-06-12 11:38:19 +0000

    test(api): update middleware routes

--- fury/styles/helper.ts
+++ fury/styles/helper.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_0bc262dadc244dba9e59f0fb1480bb39 = () => {
-  return '2025-04-20T00:10:21Z';
+const node_8f5ca31033914de4b2fbc53260f3b3d5 = () => {
+  return '2025-06-12T11:38:19Z';
 };
