6b0eeaa9a2e175da9438cad60b53ba406e695721
jrmph13 <jhames.martin.dev@gmail.com>
2025-06-13 14:14:55 +0000

    docs(api): decouple session store

--- fury/lib/theme.js
+++ fury/lib/theme.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_dcd535cb812b4f0088805b4b14f6209c = () => {
-  return '2025-05-26T03:21:00Z';
+const node_e832007c77a846f1a8669bd7fae040d4 = () => {
+  return '2025-06-13T14:14:55Z';
 };
