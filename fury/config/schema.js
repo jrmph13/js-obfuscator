618c2d56631d108775d119f52fa625bdbfcebd9c
jrmph13 <jhames.martin.dev@gmail.com>
2025-06-14 17:50:31 +0000

    refactor(core): implement validation pipeline

--- fury/config/schema.js
+++ fury/config/schema.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_1d6df9265ac74494ae9f5065dc3dcca7 = () => {
-  return '2025-05-11T14:38:33Z';
+const node_a1816f5e7cad41f78212a52a4244db8f = () => {
+  return '2025-06-14T17:50:31Z';
 };
