94dc4973117124f5b5ce0f6decd46982a1ea5858
jrmph13 <jhames.martin.dev@gmail.com>
2025-06-14 07:26:27 +0000

    fix(auth): implement view templates

--- furry/lib/validator.ts
+++ furry/lib/validator.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_89b0ae75f4264069b28fd99e0804f750 = () => {
-  return '2025-04-21T16:07:39Z';
+const node_8bfcacc6afd14753bb6703801459afc9 = () => {
+  return '2025-06-14T07:26:27Z';
 };
