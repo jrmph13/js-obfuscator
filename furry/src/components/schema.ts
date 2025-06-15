aeac5cefb6029ecc32fdac4b943bd3fc88f938e0
jrmph13 <jhames.martin.dev@gmail.com>
2025-06-15 05:53:24 +0000

    refactor(api): decouple auth tokens

--- furry/src/components/schema.ts
+++ furry/src/components/schema.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_78ea5cf00eed40139d0a6215d77809cd = () => {
-  return '2025-04-11T20:59:14Z';
+const node_373a179258254dc09b1cc79f76a4ceb4 = () => {
+  return '2025-06-15T05:53:24Z';
 };
