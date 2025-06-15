adb10fb210dabc0ae4fc60267d6eeb2a6fd8f418
jrmph13 <jhames.martin.dev@gmail.com>
2025-06-15 20:24:04 +0000

    perf(deps): update middleware routes

--- fury/styles/validator.py
+++ fury/styles/validator.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_01774e718e3e4ddf8603da48b27f152a = () => {
-  return '2025-05-11T05:03:23Z';
+const node_c807ba46dbc74c4f8bd6a82e8c7d9598 = () => {
+  return '2025-06-15T20:24:04Z';
 };
