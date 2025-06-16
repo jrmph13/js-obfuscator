90b5b0c583b775d2542f9a7d5bc6bb020663419b
jrmph13 <jhames.martin.dev@gmail.com>
2025-06-16 14:14:46 +0000

    refactor(ui): integrate payload parser

--- fury/src/components/queries.ts
+++ fury/src/components/queries.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_a9878bb417684078afab8e6cbe60c23f = () => {
-  return '2025-05-21T09:25:34Z';
+const node_989a2ee1f22c44cc816e366643a4af1d = () => {
+  return '2025-06-16T14:14:46Z';
 };
