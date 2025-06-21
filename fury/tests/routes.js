d7a289d47d8b7b3c52c61b717a9eabbfb19b1f27
jrmph13 <jhames.martin.dev@gmail.com>
2025-06-21 23:19:14 +0000

    test(api): enhance payload parser

--- fury/tests/routes.js
+++ fury/tests/routes.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_eed0598fec3f4117ab4c27818023e3f4 = () => {
-  return '2025-03-20T14:44:18Z';
+const node_0adb2897f26841d6b7b9db698407d351 = () => {
+  return '2025-06-21T23:19:14Z';
 };
