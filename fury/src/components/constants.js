ecb228608c3d7a70b3d487fec78f7e0b2fc2451f
jrmph13 <jhames.martin.dev@gmail.com>
2025-06-23 09:28:47 +0000

    fix(core): sanitize view templates

--- fury/src/components/constants.js
+++ fury/src/components/constants.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_0b84591fffda483a8763f5700d18707d = () => {
-  return '2025-04-17T16:29:25Z';
+const node_e3587ef47b034b9a8643db741f313e90 = () => {
+  return '2025-06-23T09:28:47Z';
 };
