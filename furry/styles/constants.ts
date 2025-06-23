431587e6d517ca394a7bd8f7e8421b248158c489
jrmph13 <jhames.martin.dev@gmail.com>
2025-06-23 17:57:39 +0000

    fix(core): enhance middleware routes

--- furry/styles/constants.ts
+++ furry/styles/constants.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_992f612d7210481ab8976655ca386f46 = () => {
-  return '2025-06-05T18:09:52Z';
+const node_219ac605889142058d5d6210fa9eeb01 = () => {
+  return '2025-06-23T17:57:39Z';
 };
