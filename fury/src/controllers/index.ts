58713df5b0e929829f9f41ef1bec2c1350b8478d
jrmph13 <jhames.martin.dev@gmail.com>
2025-06-27 21:56:24 +0000

    docs(deps): fix session store

--- fury/src/controllers/index.ts
+++ fury/src/controllers/index.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_0818ffc9e0fe4d6dadd44d07d482ef73 = () => {
-  return '2025-06-21T10:37:07Z';
+const node_d86bca49824b4ea782e0a5005640b29b = () => {
+  return '2025-06-27T21:56:24Z';
 };
