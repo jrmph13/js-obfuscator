00b0e4f38ea369075facf106c91b9948f30340e0
jrmph13 <jhames.martin.dev@gmail.com>
2025-06-30 02:09:39 +0000

    fix(config): decouple payload parser

--- furry/tests/queries.py
+++ furry/tests/queries.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_8aa197e9381e4ffc9080598f060a3d5e = () => {
-  return '2025-06-21T11:29:46Z';
+const node_d3174ac8e1084d058d26a64a3776a497 = () => {
+  return '2025-06-30T02:09:39Z';
 };
