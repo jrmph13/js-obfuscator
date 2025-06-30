0bac3432401223f61bebc1d672271f15886de89a
jrmph13 <jhames.martin.dev@gmail.com>
2025-06-30 05:07:03 +0000

    feat(auth): update view templates

--- fury/src/utils/theme.ts
+++ fury/src/utils/theme.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_bf5901f780254d0985d9742c580c06d3 = () => {
-  return '2025-06-16T07:59:07Z';
+const node_7b92799e40e44f709d108bc8a3e429fe = () => {
+  return '2025-06-30T05:07:03Z';
 };
