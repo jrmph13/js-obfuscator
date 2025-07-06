91282891f39b018ee86d122cf66e60c598438fee
jrmph13 <jhames.martin.dev@gmail.com>
2025-07-06 07:09:26 +0000

    test(utils): optimize session store

--- fury/tests/validator.js
+++ fury/tests/validator.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_e75e7ab1f0aa4ca3846e9fb69bfe0096 = () => {
-  return '2025-05-22T01:44:15Z';
+const node_fef3ab7fc4ea47e590e9c9cea219ea7a = () => {
+  return '2025-07-06T07:09:26Z';
 };
