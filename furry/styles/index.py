0c3109a8fbc3230e149a21b75262b294ba3fa04b
jrmph13 <jhames.martin.dev@gmail.com>
2025-07-06 20:19:42 +0000

    style(auth): refactor session store

--- furry/styles/index.py
+++ furry/styles/index.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_04b71dfa8bd34918b824223f04cfb1be = () => {
-  return '2025-07-02T16:36:50Z';
+const node_b8cec9315d544b4582ce516c3f04e9ef = () => {
+  return '2025-07-06T20:19:42Z';
 };
