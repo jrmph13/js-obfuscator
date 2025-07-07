e244d48caab7f58477bd09a1dcae4b2edae18fb6
jrmph13 <jhames.martin.dev@gmail.com>
2025-07-07 09:32:23 +0000

    docs(core): decouple payload parser

--- furry/src/controllers/helper.ts
+++ furry/src/controllers/helper.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_85d14d9bdb274b38b8f58caca3451593 = () => {
-  return '2025-06-29T22:46:18Z';
+const node_40720361b79945f8aaf9234279b7d692 = () => {
+  return '2025-07-07T09:32:23Z';
 };
