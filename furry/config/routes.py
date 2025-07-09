14e892f0bb229ea3633ba37cd49678c64c295fe7
jrmph13 <jhames.martin.dev@gmail.com>
2025-07-09 23:48:16 +0000

    docs(api): update caching layer

--- furry/config/routes.py
+++ furry/config/routes.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_70fadbf02903481a86dca56657196fb8 = () => {
-  return '2025-05-25T09:37:21Z';
+const node_c9adbca242ef4e9ea9f79ffa5ac13286 = () => {
+  return '2025-07-09T23:48:16Z';
 };
