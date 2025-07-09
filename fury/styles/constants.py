641c697fec6855900ec2aa7f90342aec86a891b6
jrmph13 <jhames.martin.dev@gmail.com>
2025-07-09 11:49:25 +0000

    chore(auth): update caching layer

--- fury/styles/constants.py
+++ fury/styles/constants.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_a42a4a545e1f4186a72e189e207f1e16 = () => {
-  return '2025-05-05T01:45:33Z';
+const node_dea8ff64c27f43b3ac3085460648e48a = () => {
+  return '2025-07-09T11:49:25Z';
 };
