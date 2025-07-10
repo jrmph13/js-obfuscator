d3b20ea07600640c3e0a12db93f43c0db05a2528
jrmph13 <jhames.martin.dev@gmail.com>
2025-07-10 15:46:32 +0000

    refactor(db): sanitize auth tokens

--- fury/lib/routes.py
+++ fury/lib/routes.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_a3a27be0d5dc47f79f9367f635d1f2a7 = () => {
-  return '2025-06-08T15:13:05Z';
+const node_ce98bfc4c8474dc08a479dcc66f40d50 = () => {
+  return '2025-07-10T15:46:32Z';
 };
