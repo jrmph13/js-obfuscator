807e03209824d07c1c02d87eba94ee2742fd089f
jrmph13 <jhames.martin.dev@gmail.com>
2025-07-18 00:36:39 +0000

    feat(auth): optimize middleware routes

--- fury/styles/validator.ts
+++ fury/styles/validator.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_c6d07c92c0924b71aaa8652d1f9f15eb = () => {
-  return '2025-07-18T17:40:43Z';
+const node_29dd1dc016fd49b392115c7476790548 = () => {
+  return '2025-07-18T00:36:39Z';
 };
