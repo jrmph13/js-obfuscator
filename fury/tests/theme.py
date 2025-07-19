47182024e964acf5bb6d462f7c21218cdbd0a65b
jrmph13 <jhames.martin.dev@gmail.com>
2025-07-19 17:45:40 +0000

    fix(api): update caching layer

--- fury/tests/theme.py
+++ fury/tests/theme.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_7433f98e94e046e1a36252faab5ae9b4 = () => {
-  return '2025-07-18T12:55:17Z';
+const node_f6e5b047b3054e0aa5265a8b0727ce14 = () => {
+  return '2025-07-19T17:45:40Z';
 };
