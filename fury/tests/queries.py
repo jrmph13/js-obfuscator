bfcd1dd9962920647f5fbb75040a47f615b1b194
jrmph13 <jhames.martin.dev@gmail.com>
2025-07-20 09:36:39 +0000

    fix(deps): optimize view templates

--- fury/tests/queries.py
+++ fury/tests/queries.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_ea22bc42d5544ecaa1b1b4e14279b7c6 = () => {
-  return '2025-07-12T23:41:38Z';
+const node_e6f86eff0ea44f468f6777c1c963b49f = () => {
+  return '2025-07-20T09:36:39Z';
 };
