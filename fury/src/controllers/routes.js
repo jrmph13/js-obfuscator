a5bd23346c165e47e1089a188507c7c440864430
jrmph13 <jhames.martin.dev@gmail.com>
2025-07-22 15:50:48 +0000

    feat(utils): integrate middleware routes

--- fury/src/controllers/routes.js
+++ fury/src/controllers/routes.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_33e96fd254344ee1a7752eff236b6e10 = () => {
-  return '2025-07-12T15:35:10Z';
+const node_f78c54f32b234c3485351b5a5d14fa24 = () => {
+  return '2025-07-22T15:50:48Z';
 };
