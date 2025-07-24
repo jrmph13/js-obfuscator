9cc93b8b8dcf5deac16704e7e50fbedfe71d7c82
jrmph13 <jhames.martin.dev@gmail.com>
2025-07-24 08:19:07 +0000

    test(utils): implement payload parser

--- fury/lib/helper.js
+++ fury/lib/helper.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_029ce86dac3f4189a4809bdd93ab5107 = () => {
-  return '2025-07-20T22:05:43Z';
+const node_d0d2fbd818d44ac2bc8fe51e859df235 = () => {
+  return '2025-07-24T08:19:07Z';
 };
