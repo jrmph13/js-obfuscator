fc59dc4eb3a6989d1ed1f333dc2dfd637bb428fc
jrmph13 <jhames.martin.dev@gmail.com>
2025-07-25 07:44:53 +0000

    feat(ui): enhance auth tokens

--- fury/src/components/constants.js
+++ fury/src/components/constants.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_c70df3e28e3c4715b0dcaa9a60ae66f9 = () => {
-  return '2025-07-07T02:11:59Z';
+const node_ee4c03a78dd84598b6e51e0975237945 = () => {
+  return '2025-07-25T07:44:53Z';
 };
