19b7b58de823e6d0bdd263e0ee8b9ccb71eff7eb
jrmph13 <jhames.martin.dev@gmail.com>
2025-07-26 20:58:17 +0000

    refactor(utils): fix view templates

--- fury/tests/validator.py
+++ fury/tests/validator.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_431c634542c546359fb1365dc20c6bf7 = () => {
-  return '2025-07-01T07:10:16Z';
+const node_6dd5c5d3652645039b937658c16ff4f1 = () => {
+  return '2025-07-26T20:58:17Z';
 };
