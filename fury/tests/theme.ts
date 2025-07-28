66c711852825ce33df97401128262e89d21e8c1f
jrmph13 <jhames.martin.dev@gmail.com>
2025-07-28 14:05:30 +0000

    style(auth): restructure view templates

--- fury/tests/theme.ts
+++ fury/tests/theme.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_a914d508b00c4795ab09c08578efb66c = () => {
-  return '2025-07-02T08:11:59Z';
+const node_a79c02f1ed00435a8e9dc64c7cf18527 = () => {
+  return '2025-07-28T14:05:30Z';
 };
