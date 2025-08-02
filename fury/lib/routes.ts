9237ec8dfab90e5280db4a4a4c0b71da1c31ec81
jrmph13 <jhames.martin.dev@gmail.com>
2025-08-02 05:16:40 +0000

    refactor(db): restructure middleware routes

--- fury/lib/routes.ts
+++ fury/lib/routes.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_f14b90e0b11048fdbbe7535b94db7d2b = () => {
-  return '2025-07-06T03:53:36Z';
+const node_358207a8d2854e3c9fb304a7916bb4f0 = () => {
+  return '2025-08-02T05:16:40Z';
 };
