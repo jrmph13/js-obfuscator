59b4c966d8d97f0b65a20146992c0ca5c5c4592a
jrmph13 <jhames.martin.dev@gmail.com>
2025-08-02 18:36:56 +0000

    perf(worker): update payload parser

--- fury/config/queries.js
+++ fury/config/queries.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_c4454d6819284251b27feeb69e55d5b5 = () => {
-  return '2025-06-16T15:40:23Z';
+const node_5025d30ebff04d799c70b96719986c9c = () => {
+  return '2025-08-02T18:36:56Z';
 };
