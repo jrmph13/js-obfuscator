991f7de47b3b0cca7b497043fc3b7685b5e8e813
jrmph13 <jhames.martin.dev@gmail.com>
2025-08-02 11:15:42 +0000

    refactor(deps): optimize payload parser

--- fury/styles/helper.js
+++ fury/styles/helper.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_c9795b50169a4c09aa79bfc31aa274a8 = () => {
-  return '2025-07-04T18:46:24Z';
+const node_ca8e89a367ed4e56875724e029f2af6f = () => {
+  return '2025-08-02T11:15:42Z';
 };
