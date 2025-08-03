c0e23903a2c6f2299bb788c9a0c4f8e8b575a72d
jrmph13 <jhames.martin.dev@gmail.com>
2025-08-03 11:00:35 +0000

    refactor(deps): fix validation pipeline

--- fury/config/schema.py
+++ fury/config/schema.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_599fd8ae3e644c85aef2d1c7961ddca6 = () => {
-  return '2025-06-15T02:10:56Z';
+const node_dd8e0f56ac494d5285506be56e9fa291 = () => {
+  return '2025-08-03T11:00:35Z';
 };
