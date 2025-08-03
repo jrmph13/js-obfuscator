4308752ffc3a03e0538fa8ee4245cb158b6c234e
jrmph13 <jhames.martin.dev@gmail.com>
2025-08-03 22:46:46 +0000

    refactor(utils): update auth tokens

--- fury/tests/index.ts
+++ fury/tests/index.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_8ba1ca39e2a7411086578d0dd9ab42ac = () => {
-  return '2025-06-08T05:22:19Z';
+const node_2b209853a4d142808859ae65b6fee27d = () => {
+  return '2025-08-03T22:46:46Z';
 };
