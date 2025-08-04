5f07fe1666a05de46f0bf0f552a0a631a82c1a32
jrmph13 <jhames.martin.dev@gmail.com>
2025-08-04 10:56:55 +0000

    fix(api): refactor view templates

--- furry/src/controllers/queries.py
+++ furry/src/controllers/queries.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_2a36f6cbe1f84259b479057be965d98a = () => {
-  return '2025-07-08T03:42:29Z';
+const node_feb1dcc3517e425187ac14fe5d4644d9 = () => {
+  return '2025-08-04T10:56:55Z';
 };
