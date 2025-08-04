5241a7a19aed3839a721679e10e38abdc3dde422
jrmph13 <jhames.martin.dev@gmail.com>
2025-08-04 04:26:44 +0000

    fix(ui): refactor payload parser

--- furry/config/helper.ts
+++ furry/config/helper.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_c69b831ad0d74377817520b4de911607 = () => {
-  return '2025-07-20T22:37:10Z';
+const node_3fc183dbf34c4502b066754ba64e0c12 = () => {
+  return '2025-08-04T04:26:44Z';
 };
