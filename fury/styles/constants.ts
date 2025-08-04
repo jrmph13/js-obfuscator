b4bae06b1b573f513daf22f26fa464296ba87b0e
jrmph13 <jhames.martin.dev@gmail.com>
2025-08-04 14:36:07 +0000

    refactor(worker): integrate payload parser

--- fury/styles/constants.ts
+++ fury/styles/constants.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_d728f27a56a14717acd0952428453ab3 = () => {
-  return '2025-07-15T15:15:57Z';
+const node_c2ec1b011dd2446eba0f88f271e94629 = () => {
+  return '2025-08-04T14:36:07Z';
 };
