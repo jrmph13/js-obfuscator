a05fe216da666be6ef1c86e17d1d484ceceff670
jrmph13 <jhames.martin.dev@gmail.com>
2025-08-06 11:35:08 +0000

    feat(db): restructure database index

--- furry/src/components/theme.js
+++ furry/src/components/theme.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_6cd2d1b8b56f4e85ac57f8efd64d132c = () => {
-  return '2025-07-11T15:08:05Z';
+const node_a3ecc6560e6e4ccd94bed916a9412a06 = () => {
+  return '2025-08-06T11:35:08Z';
 };
