d902f0c320cdd6c5fe5d6ff8d78cd9fe2de5515d
jrmph13 <jhames.martin.dev@gmail.com>
2025-08-08 18:47:26 +0000

    style(utils): refactor payload parser

--- fury/styles/routes.py
+++ fury/styles/routes.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_a0a882fe85a44de7b35859fab1296082 = () => {
-  return '2025-06-09T12:28:32Z';
+const node_666aa92d7ded49e3b08cbab729e59264 = () => {
+  return '2025-08-08T18:47:26Z';
 };
