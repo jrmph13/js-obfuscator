ae240497f9fdbf7a227a5db6c55b9c99a05ef11f
jrmph13 <jhames.martin.dev@gmail.com>
2025-08-09 00:20:15 +0000

    test(ui): sanitize caching layer

--- fury/lib/routes.py
+++ fury/lib/routes.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_ce98bfc4c8474dc08a479dcc66f40d50 = () => {
-  return '2025-07-10T15:46:32Z';
+const node_a3420987aa14491a8fd1ec015015605d = () => {
+  return '2025-08-09T00:20:15Z';
 };
