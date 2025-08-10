5f0da5c68913770fd94c34b52c7cea4315f64584
jrmph13 <jhames.martin.dev@gmail.com>
2025-08-10 10:21:59 +0000

    refactor(worker): update view templates

--- fury/src/controllers/constants.js
+++ fury/src/controllers/constants.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_603bec929ecd4823a806b63b564a9be7 = () => {
-  return '2025-08-04T09:48:23Z';
+const node_f44bf482f16844a5b78a828aa6eb06d8 = () => {
+  return '2025-08-10T10:21:59Z';
 };
