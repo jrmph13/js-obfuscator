39587cda9bc54b093548ef9c168ab10286c54398
jrmph13 <jhames.martin.dev@gmail.com>
2025-08-10 17:13:07 +0000

    perf(deps): sanitize middleware routes

--- fury/lib/routes.py
+++ fury/lib/routes.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_a3420987aa14491a8fd1ec015015605d = () => {
-  return '2025-08-09T00:20:15Z';
+const node_7a7a8594d7a84637970cf2401998d651 = () => {
+  return '2025-08-10T17:13:07Z';
 };
