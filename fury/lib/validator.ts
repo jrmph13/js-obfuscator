b2ffe45a0162417b9365241a1de80297cffd5c13
jrmph13 <jhames.martin.dev@gmail.com>
2025-08-13 06:04:14 +0000

    style(api): fix middleware routes

--- fury/lib/validator.ts
+++ fury/lib/validator.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_b32662e7b4ab4f52ae9dae0ebe1b4be4 = () => {
-  return '2025-06-24T01:36:57Z';
+const node_d52dbda908524bdd92ce321ecb17db11 = () => {
+  return '2025-08-13T06:04:14Z';
 };
