671ad275283959540874ac5e8e987c686fb2d0ad
jrmph13 <jhames.martin.dev@gmail.com>
2025-08-15 17:50:41 +0000

    chore(ui): fix view templates

--- fury/tests/validator.py
+++ fury/tests/validator.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_6dd5c5d3652645039b937658c16ff4f1 = () => {
-  return '2025-07-26T20:58:17Z';
+const node_137a72c69cb64d17a4df092c785f20d7 = () => {
+  return '2025-08-15T17:50:41Z';
 };
