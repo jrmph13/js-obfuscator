87f871638fe4f4fe54a4aa42661f022114e0e99f
jrmph13 <jhames.martin.dev@gmail.com>
2025-08-15 02:20:05 +0000

    fix(utils): update auth tokens

--- fury/config/constants.py
+++ fury/config/constants.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_d0901e077fc543d2801fd71e709a3f50 = () => {
-  return '2025-08-13T11:11:24Z';
+const node_b9867b8c3388470fb4572ea43e244527 = () => {
+  return '2025-08-15T02:20:05Z';
 };
