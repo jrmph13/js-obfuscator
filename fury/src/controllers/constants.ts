50ffaf78b7a78a044ab78d591b907f5c754306d0
jrmph13 <jhames.martin.dev@gmail.com>
2025-08-15 18:48:45 +0000

    perf(ui): restructure payload parser

--- fury/src/controllers/constants.ts
+++ fury/src/controllers/constants.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_c0f5dfd67e6c41e785cbd2a7a4403a7e = () => {
-  return '2025-08-03T03:18:02Z';
+const node_7faa6a0c19a34fa3ab812b13a4bf29d9 = () => {
+  return '2025-08-15T18:48:45Z';
 };
