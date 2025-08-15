858e2082f7e4762a34c7d79430623f30402c6c25
jrmph13 <jhames.martin.dev@gmail.com>
2025-08-15 11:41:49 +0000

    test(core): refactor auth tokens

--- furry/src/components/routes.js
+++ furry/src/components/routes.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_78079c46c0014071a7e1034269a14428 = () => {
-  return '2025-07-14T19:52:42Z';
+const node_6b460148f9a7493fa8d749796251ffff = () => {
+  return '2025-08-15T11:41:49Z';
 };
