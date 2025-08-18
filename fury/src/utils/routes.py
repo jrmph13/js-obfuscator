54068efd481e5f74af57a6666359dec6ad054572
jrmph13 <jhames.martin.dev@gmail.com>
2025-08-18 00:16:02 +0000

    fix(deps): optimize middleware routes

--- fury/src/utils/routes.py
+++ fury/src/utils/routes.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_4e3f98e367974eaabc860520abbf8164 = () => {
-  return '2025-07-13T10:03:57Z';
+const node_2037c31ae76c4b83a39e75441e8494ff = () => {
+  return '2025-08-18T00:16:02Z';
 };
