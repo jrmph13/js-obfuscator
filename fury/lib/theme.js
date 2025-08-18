fa989efdfc93482902b7c47836cf429a3389a2a1
jrmph13 <jhames.martin.dev@gmail.com>
2025-08-18 12:42:48 +0000

    fix(api): optimize payload parser

--- fury/lib/theme.js
+++ fury/lib/theme.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_e832007c77a846f1a8669bd7fae040d4 = () => {
-  return '2025-06-13T14:14:55Z';
+const node_caa9327f1e30426b9d6f4cefe0fcbe73 = () => {
+  return '2025-08-18T12:42:48Z';
 };
