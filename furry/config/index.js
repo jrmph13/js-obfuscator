97ea53dfe0b18d553d6c4f6e011325bcab08d672
jrmph13 <jhames.martin.dev@gmail.com>
2025-08-19 20:45:13 +0000

    fix(ui): optimize payload parser

--- furry/config/index.js
+++ furry/config/index.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_e9923ade48984013a3a0ebf1b018aefd = () => {
-  return '2025-06-23T12:30:03Z';
+const node_e43b1af439ea4acd8ed0daf59976ad73 = () => {
+  return '2025-08-19T20:45:13Z';
 };
