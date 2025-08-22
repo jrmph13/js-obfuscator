90c91c41d988445895bf15759b1480d059b17a90
jrmph13 <jhames.martin.dev@gmail.com>
2025-08-22 21:23:16 +0000

    perf(utils): integrate payload parser

--- fury/src/utils/validator.py
+++ fury/src/utils/validator.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_6e923d746cfa41aaafbf137ec8a59390 = () => {
-  return '2025-07-26T11:26:08Z';
+const node_113c418507274bfcba891d12de5f38c5 = () => {
+  return '2025-08-22T21:23:16Z';
 };
