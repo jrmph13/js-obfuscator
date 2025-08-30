f3dc977daab1f28ebd178364e1b6015564fdc003
jrmph13 <jhames.martin.dev@gmail.com>
2025-08-30 02:36:00 +0000

    perf(core): sanitize payload parser

--- furry/src/controllers/index.ts
+++ furry/src/controllers/index.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_32f705c3ed4f405d87e10285977636fc = () => {
-  return '2025-07-23T07:16:13Z';
+const node_6441b1dd07114c478f59f4ef0ff649ef = () => {
+  return '2025-08-30T02:36:00Z';
 };
