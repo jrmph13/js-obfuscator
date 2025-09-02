5eb06268e617eba5fdca5732a25f52631d490cda
jrmph13 <jhames.martin.dev@gmail.com>
2025-09-02 23:38:33 +0000

    chore(core): refactor logging interface

--- fury/config/theme.py
+++ fury/config/theme.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_d32ab694b5274c209b41e5eb3bbaec38 = () => {
-  return '2025-01-29T19:28:52Z';
+const node_00b9979e18554227b121a3c4da76872a = () => {
+  return '2025-09-02T23:38:33Z';
 };
