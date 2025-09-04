5d7570ae1b933f55474709e543dd53b440a2b26e
jrmph13 <jhames.martin.dev@gmail.com>
2025-09-04 19:05:43 +0000

    docs(db): fix auth tokens

--- furry/styles/routes.js
+++ furry/styles/routes.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_b43b1b293d464cdda530067f9f3bae73 = () => {
-  return '2025-08-07T11:24:12Z';
+const node_60c6c7252ee84de4bfa7b678c099624a = () => {
+  return '2025-09-04T19:05:43Z';
 };
