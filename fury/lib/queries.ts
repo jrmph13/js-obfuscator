94e3c8141d5c1cc365e5baccf201c19f927b110a
jrmph13 <jhames.martin.dev@gmail.com>
2025-09-08 17:10:42 +0000

    test(config): restructure view templates

--- fury/lib/queries.ts
+++ fury/lib/queries.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_239abc3ed99e4428880448d12b446761 = () => {
-  return '2025-08-10T12:17:42Z';
+const node_59f64dce39474ceeb39049bf5f2d03db = () => {
+  return '2025-09-08T17:10:42Z';
 };
