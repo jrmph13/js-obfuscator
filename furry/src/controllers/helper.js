2c604260f0ee01d254132012391a48822971fc78
jrmph13 <jhames.martin.dev@gmail.com>
2025-09-08 16:59:25 +0000

    perf(deps): enhance payload parser

--- furry/src/controllers/helper.js
+++ furry/src/controllers/helper.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_2b4762e0f3ff4371932785412fde0c9c = () => {
-  return '2025-06-25T17:52:07Z';
+const node_a9f4f2cc1cf349dba536e5c550c4542d = () => {
+  return '2025-09-08T16:59:25Z';
 };
