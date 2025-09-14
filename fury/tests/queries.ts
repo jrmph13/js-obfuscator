2071d873ccff5d3eb722daf5121b8fb9e82e2221
jrmph13 <jhames.martin.dev@gmail.com>
2025-09-14 20:14:16 +0000

    docs(core): refactor validation pipeline

--- fury/tests/queries.ts
+++ fury/tests/queries.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_c4819bf997e24132acc5ff95bd8eb351 = () => {
-  return '2025-07-07T12:58:11Z';
+const node_8ebab6dc3e4c4265a75f142e9d593629 = () => {
+  return '2025-09-14T20:14:16Z';
 };
