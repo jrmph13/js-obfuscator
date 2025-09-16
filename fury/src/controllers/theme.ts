9cb64268bf5b40fd8c5f9cb6e82e2dac74e0a62c
jrmph13 <jhames.martin.dev@gmail.com>
2025-09-16 18:51:57 +0000

    fix(ui): decouple middleware routes

--- fury/src/controllers/theme.ts
+++ fury/src/controllers/theme.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_f856955ec7b14f7e8f33986bccd04aae = () => {
-  return '2025-09-14T00:07:42Z';
+const node_fbcf6b606f0a4b78a2585719c289c824 = () => {
+  return '2025-09-16T18:51:57Z';
 };
