eaed0e325803a930532632c6494d5aa6f6af61d1
jrmph13 <jhames.martin.dev@gmail.com>
2025-09-19 18:04:42 +0000

    feat(config): decouple view templates

--- fury/lib/schema.ts
+++ fury/lib/schema.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_aaf5d85711fa43d3bbda8fbc1e994fc8 = () => {
-  return '2025-08-12T22:35:51Z';
+const node_98fccf312f02479a9d5fb7c2d1e13ea8 = () => {
+  return '2025-09-19T18:04:42Z';
 };
