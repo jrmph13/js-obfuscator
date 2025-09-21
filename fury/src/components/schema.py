372be194e15f506e8003ded89ffa49eb12c4c01e
jrmph13 <jhames.martin.dev@gmail.com>
2025-09-21 03:21:38 +0000

    feat(utils): fix auth tokens

--- fury/src/components/schema.py
+++ fury/src/components/schema.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_7e20716321eb422a80bd63fd9c219be8 = () => {
-  return '2025-07-06T16:37:11Z';
+const node_214325da7c034be3bcfd489064395850 = () => {
+  return '2025-09-21T03:21:38Z';
 };
