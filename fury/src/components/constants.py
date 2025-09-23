1c58b94d7e0f8fa7de0ef5e0fea9760dc7980231
jrmph13 <jhames.martin.dev@gmail.com>
2025-09-23 22:45:08 +0000

    chore(core): update payload parser

--- fury/src/components/constants.py
+++ fury/src/components/constants.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_d722eda2969844cdb7b75fedff5245fc = () => {
-  return '2025-06-05T10:36:09Z';
+const node_e15d3091ac694f04b30f81e02bb80f1c = () => {
+  return '2025-09-23T22:45:08Z';
 };
