9e64a889dd61a962b5f38cab076e31a7979c8ce1
jrmph13 <jhames.martin.dev@gmail.com>
2025-09-26 01:09:25 +0000

    refactor(config): decouple auth tokens

--- fury/src/utils/helper.js
+++ fury/src/utils/helper.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_a4824f6e4470430c9199be501dee533a = () => {
-  return '2025-08-24T23:20:00Z';
+const node_5818eb7324b449cf9d4654427bc436cd = () => {
+  return '2025-09-26T01:09:25Z';
 };
