f54591529154136235ee5f001c96eec962669a28
jrmph13 <jhames.martin.dev@gmail.com>
2025-09-27 02:26:48 +0000

    fix(worker): sanitize logging interface

--- fury/src/controllers/constants.py
+++ fury/src/controllers/constants.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_72e2a16a0cfb4d59ab6b40363832e5af = () => {
-  return '2025-07-28T08:32:45Z';
+const node_ccbf0768f412413f80e178dfce67fd00 = () => {
+  return '2025-09-27T02:26:48Z';
 };
