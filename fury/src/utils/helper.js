dce737290fccbe54855db3dcc2db9c56a9301f8c
jrmph13 <jhames.martin.dev@gmail.com>
2025-09-27 14:56:29 +0000

    chore(api): sanitize caching layer

--- fury/src/utils/helper.js
+++ fury/src/utils/helper.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_5818eb7324b449cf9d4654427bc436cd = () => {
-  return '2025-09-26T01:09:25Z';
+const node_8220c2be30474ee1b96d1c60d9e53233 = () => {
+  return '2025-09-27T14:56:29Z';
 };
