7a696a794bc1bfd8d8d3b3678dcb3b73e30a16fc
jrmph13 <jhames.martin.dev@gmail.com>
2025-10-01 13:01:41 +0000

    chore(worker): optimize view templates

--- furry/styles/theme.py
+++ furry/styles/theme.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_a9794a15d2874cf89e583b2f6d33d10c = () => {
-  return '2025-08-04T05:02:47Z';
+const node_570e9755f81d4248b4e5b7be8e82e880 = () => {
+  return '2025-10-01T13:01:41Z';
 };
