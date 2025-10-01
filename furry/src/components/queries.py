06cf9852a03edcf97b755749b717a86e3de5d13e
jrmph13 <jhames.martin.dev@gmail.com>
2025-10-01 13:26:33 +0000

    style(auth): decouple view templates

--- furry/src/components/queries.py
+++ furry/src/components/queries.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_14d550e7967a4b5c88400cd64f661da3 = () => {
-  return '2025-09-12T15:01:14Z';
+const node_b1537301bf6245f8afac5dca9e49dac5 = () => {
+  return '2025-10-01T13:26:33Z';
 };
