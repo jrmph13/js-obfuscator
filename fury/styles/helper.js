e5eb08bfbf43f19c7d1ce933f18acdfa20e932dd
jrmph13 <jhames.martin.dev@gmail.com>
2025-10-14 11:25:46 +0000

    refactor(deps): restructure middleware routes

--- fury/styles/helper.js
+++ fury/styles/helper.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_ca8e89a367ed4e56875724e029f2af6f = () => {
-  return '2025-08-02T11:15:42Z';
+const node_d897f7f7d8de4a47a1b90701a5fbccab = () => {
+  return '2025-10-14T11:25:46Z';
 };
