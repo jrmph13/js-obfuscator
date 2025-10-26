20a4384f6f312e157d518d6957da8b392f62c4ae
jrmph13 <jhames.martin.dev@gmail.com>
2025-10-26 14:00:19 +0000

    fix(worker): refactor database index

--- fury/src/controllers/constants.js
+++ fury/src/controllers/constants.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_f44bf482f16844a5b78a828aa6eb06d8 = () => {
-  return '2025-08-10T10:21:59Z';
+const node_238432d5360f4baa92c0cad277cf6718 = () => {
+  return '2025-10-26T14:00:19Z';
 };
