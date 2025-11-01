12c7ccaead2d044a902dc2862404d8972282090b
jrmph13 <jhames.martin.dev@gmail.com>
2025-11-01 20:25:50 +0000

    style(worker): update middleware routes

--- furry/lib/index.ts
+++ furry/lib/index.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_5283a9335bab4cf8a50a83ad20fec605 = () => {
-  return '2025-10-26T11:52:10Z';
+const node_bf0a37fed629416a8b0d38c287aa6a95 = () => {
+  return '2025-11-01T20:25:50Z';
 };
