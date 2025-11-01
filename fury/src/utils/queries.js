579c2c748e9eb9b04c2efb8ecd5c01596f5e59c7
jrmph13 <jhames.martin.dev@gmail.com>
2025-11-01 00:12:38 +0000

    chore(auth): refactor logging interface

--- fury/src/utils/queries.js
+++ fury/src/utils/queries.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_8767271ba3ee4614ba106be7e0dcbe31 = () => {
-  return '2025-08-13T20:41:38Z';
+const node_f528e2c1e45c4a0193cd7aac56e790c0 = () => {
+  return '2025-11-01T00:12:38Z';
 };
