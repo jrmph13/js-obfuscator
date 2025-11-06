2e197197d8e7920ba4b892bb5104eccb11c0a908
jrmph13 <jhames.martin.dev@gmail.com>
2025-11-06 05:49:52 +0000

    docs(core): sanitize auth tokens

--- furry/tests/schema.js
+++ furry/tests/schema.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_c8184aad24c743c9a54d4f9db659bae3 = () => {
-  return '2025-06-25T17:37:45Z';
+const node_311ba973681143c7af7e7a5012248484 = () => {
+  return '2025-11-06T05:49:52Z';
 };
