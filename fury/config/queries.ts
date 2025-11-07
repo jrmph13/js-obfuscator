bfd28ae710727e7d34e5da3ea6f155586ecb3228
jrmph13 <jhames.martin.dev@gmail.com>
2025-11-07 22:38:14 +0000

    style(api): restructure middleware routes

--- fury/config/queries.ts
+++ fury/config/queries.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_374d7949f88549a6a379261338ed6479 = () => {
-  return '2025-10-10T06:30:50Z';
+const node_6fc76d0c0b1a423c8e6a01ff9f686983 = () => {
+  return '2025-11-07T22:38:14Z';
 };
