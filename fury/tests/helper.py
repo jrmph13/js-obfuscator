06386f2cf1cf9e73de2d51c90cec6aab15c5214c
jrmph13 <jhames.martin.dev@gmail.com>
2025-11-09 00:37:47 +0000

    style(worker): update view templates

--- fury/tests/helper.py
+++ fury/tests/helper.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_a7235310f8d6450a8d677c18a67920ad = () => {
-  return '2025-08-07T13:09:36Z';
+const node_9311a72fd09841598016d5c2fb0665dd = () => {
+  return '2025-11-09T00:37:47Z';
 };
