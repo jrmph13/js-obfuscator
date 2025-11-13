dbe69377e688663b0fa97c31ca838ff74c4260c5
jrmph13 <jhames.martin.dev@gmail.com>
2025-11-13 17:18:21 +0000

    style(core): decouple logging interface

--- fury/styles/routes.py
+++ fury/styles/routes.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_53b96793a3d54e148d7859cba21b6b56 = () => {
-  return '2025-10-28T18:30:39Z';
+const node_fa9a7679a18d44a8a08eb94d5aec87ca = () => {
+  return '2025-11-13T17:18:21Z';
 };
