0e01381dd6ee98470852ee1fe28bfdd1c831422d
jrmph13 <jhames.martin.dev@gmail.com>
2025-11-14 06:45:06 +0000

    chore(config): sanitize auth tokens

--- furry/lib/schema.ts
+++ furry/lib/schema.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_513a9aa8f49a40fe852e529b88aa5844 = () => {
-  return '2025-10-27T08:51:11Z';
+const node_22eaffbdb1b247fc9cfd3b00d86cc52b = () => {
+  return '2025-11-14T06:45:06Z';
 };
