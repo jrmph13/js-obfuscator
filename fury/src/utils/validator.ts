534eb159c406b4b1b78860554dd85a75a5f9cf7c
jrmph13 <jhames.martin.dev@gmail.com>
2025-11-14 15:35:32 +0000

    docs(ui): sanitize payload parser

--- fury/src/utils/validator.ts
+++ fury/src/utils/validator.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_d7ea1a0e9b1f40afb3e5a92227d8c169 = () => {
-  return '2025-10-27T11:49:08Z';
+const node_85d3a968b0cb48729b87622d06cce0f7 = () => {
+  return '2025-11-14T15:35:32Z';
 };
