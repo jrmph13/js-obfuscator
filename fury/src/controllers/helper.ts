831b53034b54cc7cf2dae477bb3c29169b2896da
jrmph13 <jhames.martin.dev@gmail.com>
2025-11-16 09:37:08 +0000

    test(auth): restructure payload parser

--- fury/src/controllers/helper.ts
+++ fury/src/controllers/helper.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_b8bca3b5bd5d49f597fd6afb1e39d42c = () => {
-  return '2025-11-12T11:25:45Z';
+const node_1e03a07b4aaa40ca9b3a39f0bfefd72a = () => {
+  return '2025-11-16T09:37:08Z';
 };
