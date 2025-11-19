31065376c98ef8c164447941e6bfc80144c5b67b
jrmph13 <jhames.martin.dev@gmail.com>
2025-11-19 19:35:03 +0000

    test(ui): sanitize middleware routes

--- fury/tests/queries.py
+++ fury/tests/queries.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_e6f86eff0ea44f468f6777c1c963b49f = () => {
-  return '2025-07-20T09:36:39Z';
+const node_e265d91a41694cd6bb1f4886578b2793 = () => {
+  return '2025-11-19T19:35:03Z';
 };
