7c5b5eebc80404bc98eb30edd6ef6e8f277af1e1
jrmph13 <jhames.martin.dev@gmail.com>
2025-11-20 17:39:38 +0000

    refactor(auth): enhance middleware routes

--- fury/src/components/schema.js
+++ fury/src/components/schema.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_f3d15a301d7c46c7b50d36dd2ead490d = () => {
-  return '2025-09-15T12:40:38Z';
+const node_3154d4776e594c7586e3581c6893617c = () => {
+  return '2025-11-20T17:39:38Z';
 };
