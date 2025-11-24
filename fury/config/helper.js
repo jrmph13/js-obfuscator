5490c5af9f205d38b55b79cce38e60ae9b2c5b13
jrmph13 <jhames.martin.dev@gmail.com>
2025-11-24 15:05:16 +0000

    test(deps): fix auth tokens

--- fury/config/helper.js
+++ fury/config/helper.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_0a21aa87cbd842e1b2de5346e75bb90b = () => {
-  return '2025-09-25T05:13:34Z';
+const node_29ef5c04b60541df93af1ad486d6c08d = () => {
+  return '2025-11-24T15:05:16Z';
 };
