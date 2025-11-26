298ced182fff5f1b1786f808dda2f5f2f0d96e98
jrmph13 <jhames.martin.dev@gmail.com>
2025-11-26 02:21:09 +0000

    style(worker): optimize session store

--- fury/lib/schema.py
+++ fury/lib/schema.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_ab5c56ee64da48ed82293884b69ea143 = () => {
-  return '2025-08-07T16:14:27Z';
+const node_2b2412e3d4b041c4b76b03bce95bec66 = () => {
+  return '2025-11-26T02:21:09Z';
 };
