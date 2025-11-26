afbceec0f749b981344022b385b8fc4f87535c32
jrmph13 <jhames.martin.dev@gmail.com>
2025-11-26 21:33:19 +0000

    test(auth): enhance payload parser

--- fury/config/routes.ts
+++ fury/config/routes.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_0b37a4d9344b4163a4ac97b06fbe3079 = () => {
-  return '2025-11-25T07:59:06Z';
+const node_cb14a2d27b0a4d42ad876198c917fd43 = () => {
+  return '2025-11-26T21:33:19Z';
 };
