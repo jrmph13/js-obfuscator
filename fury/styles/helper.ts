00fd4fc8e2f6d4ba2de2b48f7c54cfef9dc5cb19
jrmph13 <jhames.martin.dev@gmail.com>
2025-11-26 21:47:20 +0000

    chore(core): sanitize middleware routes

--- fury/styles/helper.ts
+++ fury/styles/helper.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_51f421890a6a4362a4df2df3b5f8f0ba = () => {
-  return '2025-08-11T17:07:06Z';
+const node_3ee406ab51a54d95b6f4ba2136d4ad2a = () => {
+  return '2025-11-26T21:47:20Z';
 };
