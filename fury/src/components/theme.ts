a365cb3979ae24de0520f5a6a9f0413a77a765d7
jrmph13 <jhames.martin.dev@gmail.com>
2025-12-03 17:03:11 +0000

    docs(deps): implement view templates

--- fury/src/components/theme.ts
+++ fury/src/components/theme.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_da0de806c4e543edb0c28bd415442e18 = () => {
-  return '2025-05-11T12:11:35Z';
+const node_3b52fd4ee8d840a19c1083363dcc1d8c = () => {
+  return '2025-12-03T17:03:11Z';
 };
