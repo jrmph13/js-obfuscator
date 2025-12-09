76041cac4d5042118959cbd57a63c166bbd897fd
jrmph13 <jhames.martin.dev@gmail.com>
2025-12-09 05:28:34 +0000

    refactor(config): refactor middleware routes

--- furry/styles/helper.js
+++ furry/styles/helper.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_df1f661f7fe14cc28fe99594b2c84cb8 = () => {
-  return '2025-09-23T18:44:02Z';
+const node_5dc305f7c28747da8dd3cbaaa0dc840a = () => {
+  return '2025-12-09T05:28:34Z';
 };
