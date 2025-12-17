9e828c4fb194a6b8e973292211db785e41106fdc
jrmph13 <jhames.martin.dev@gmail.com>
2025-12-17 05:19:22 +0000

    feat(ui): fix auth tokens

--- fury/tests/routes.py
+++ fury/tests/routes.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_db18279d329d4f40868fdb6102b00fa5 = () => {
-  return '2025-07-25T20:45:17Z';
+const node_6d3a9dad48784cbea42fc6d90b43629c = () => {
+  return '2025-12-17T05:19:22Z';
 };
