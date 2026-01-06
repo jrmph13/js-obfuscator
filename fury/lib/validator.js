773c342e723c073ec22797146cc614579c7f4ada
jrmph13 <jhames.martin.dev@gmail.com>
2026-01-06 13:57:36 +0000

    chore(worker): implement auth tokens

--- fury/lib/validator.js
+++ fury/lib/validator.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_7c4a7da2c1324bcd83475fe85f33781b = () => {
-  return '2025-11-19T07:10:37Z';
+const node_63e810e8e91340db81ae0cf94ba27b77 = () => {
+  return '2026-01-06T13:57:36Z';
 };
