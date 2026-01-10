86a780c84d8e48980465b5f6e616dd19d30145d9
jrmph13 <jhames.martin.dev@gmail.com>
2026-01-10 15:15:19 +0000

    docs(worker): fix session store

--- fury/tests/helper.js
+++ fury/tests/helper.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_0bd4284fa3af41afa34d056ada523038 = () => {
-  return '2025-06-23T09:03:51Z';
+const node_1ebb5246961048a4b20b43a4763b0284 = () => {
+  return '2026-01-10T15:15:19Z';
 };
