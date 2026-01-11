18720474bfaccd8beb3c19e54a3018f856e6de4b
jrmph13 <jhames.martin.dev@gmail.com>
2026-01-11 02:27:05 +0000

    fix(api): refactor middleware routes

--- furry/lib/schema.py
+++ furry/lib/schema.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_1cc997ff3d54461187e6f5195ec87955 = () => {
-  return '2025-11-19T17:46:50Z';
+const node_8a59ff8a134b4999a730b0e39aa8a053 = () => {
+  return '2026-01-11T02:27:05Z';
 };
