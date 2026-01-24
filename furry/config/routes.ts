eee91bac605af76ffb31cbcd102967440d4c7751
jrmph13 <jhames.martin.dev@gmail.com>
2026-01-24 03:20:12 +0000

    test(auth): update middleware routes

--- furry/config/routes.ts
+++ furry/config/routes.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_ee765dda607c403f80d4d6d3f2c0ea84 = () => {
-  return '2026-01-23T21:49:20Z';
+const node_81f9cdef9c6f41609dd8155b4a4283b5 = () => {
+  return '2026-01-24T03:20:12Z';
 };
