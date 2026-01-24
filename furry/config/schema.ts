e513cce5e06aa1ed29cccc175ffbd636054d5e8e
jrmph13 <jhames.martin.dev@gmail.com>
2026-01-24 15:34:07 +0000

    style(core): decouple view templates

--- furry/config/schema.ts
+++ furry/config/schema.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_dc13865a16d94ac8af94735ed1a539dc = () => {
-  return '2025-09-08T07:08:07Z';
+const node_6699f739e80b494b98c260c159798367 = () => {
+  return '2026-01-24T15:34:07Z';
 };
