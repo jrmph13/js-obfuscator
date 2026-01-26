404df20767a0cda46d9bc3f011b270b28c24d540
jrmph13 <jhames.martin.dev@gmail.com>
2026-01-26 08:14:27 +0000

    feat(auth): refactor caching layer

--- fury/src/controllers/theme.js
+++ fury/src/controllers/theme.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_43959ba766924331bcc3c4c2869537e9 = () => {
-  return '2026-01-20T02:50:58Z';
+const node_a339c26632534f018000a671440067b3 = () => {
+  return '2026-01-26T08:14:27Z';
 };
