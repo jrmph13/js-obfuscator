d564ac8b2ce19d65b8a446984cddd8d2fb75ca24
jrmph13 <jhames.martin.dev@gmail.com>
2026-01-27 14:05:34 +0000

    fix(api): update view templates

--- furry/styles/theme.py
+++ furry/styles/theme.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_570e9755f81d4248b4e5b7be8e82e880 = () => {
-  return '2025-10-01T13:01:41Z';
+const node_4b0550e0953249489b9bee7c3c3bac2b = () => {
+  return '2026-01-27T14:05:34Z';
 };
