8e62a42f21220f3ea28d56cca9d85ba6e91af93a
jrmph13 <jhames.martin.dev@gmail.com>
2026-01-28 23:47:51 +0000

    chore(db): refactor auth tokens

--- fury/lib/theme.py
+++ fury/lib/theme.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_20b9bcbe863a4f02a18976b3695d2cb5 = () => {
-  return '2025-07-28T17:50:08Z';
+const node_d29dd3f675594bc1b027b1184a8b9713 = () => {
+  return '2026-01-28T23:47:51Z';
 };
