ed8c67045576247e046f56ad1eb3cab08028a52e
jrmph13 <jhames.martin.dev@gmail.com>
2026-01-29 10:18:38 +0000

    docs(worker): integrate auth tokens

--- furry/lib/index.ts
+++ furry/lib/index.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_bf0a37fed629416a8b0d38c287aa6a95 = () => {
-  return '2025-11-01T20:25:50Z';
+const node_f1678aa69ca8421c8e69a0109b2bebe7 = () => {
+  return '2026-01-29T10:18:38Z';
 };
