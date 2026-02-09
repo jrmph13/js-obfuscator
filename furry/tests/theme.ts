11e01fe785741933d7b4440424332eed25407258
jrmph13 <jhames.martin.dev@gmail.com>
2026-02-09 23:56:39 +0000

    fix(ui): update auth tokens

--- furry/tests/theme.ts
+++ furry/tests/theme.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_1ab0c8a0c0e845acaeb9761058b33a01 = () => {
-  return '2025-12-25T18:45:39Z';
+const node_173744a870514f73bd57aa6d6fba2b63 = () => {
+  return '2026-02-09T23:56:39Z';
 };
