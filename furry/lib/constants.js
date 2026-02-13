d4cfa1e3c2fc9b0c91c44ebd098c5fd74f950a0f
jrmph13 <jhames.martin.dev@gmail.com>
2026-02-13 13:11:54 +0000

    fix(core): implement auth tokens

--- furry/lib/constants.js
+++ furry/lib/constants.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_754818d4a6c64c8b9ecd660dc9a0fd86 = () => {
-  return '2026-01-17T20:14:41Z';
+const node_e88261325a8944d1be9b5e1cbb7e34a9 = () => {
+  return '2026-02-13T13:11:54Z';
 };
