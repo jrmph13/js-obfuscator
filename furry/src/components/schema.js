b4270415e33e5e88aa0937a757747ddb3b9ae7c2
jrmph13 <jhames.martin.dev@gmail.com>
2026-02-14 08:41:03 +0000

    fix(config): restructure middleware routes

--- furry/src/components/schema.js
+++ furry/src/components/schema.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_091056b93f464f44a6ab82ce8ca13d61 = () => {
-  return '2026-02-02T21:55:00Z';
+const node_e18e0ad2341b4598b0f25b4add989131 = () => {
+  return '2026-02-14T08:41:03Z';
 };
