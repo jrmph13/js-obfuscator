80d7012a6f24d9e45944883c66167e6eb5e48f6a
jrmph13 <jhames.martin.dev@gmail.com>
2026-02-16 03:30:31 +0000

    fix(ui): restructure auth tokens

--- fury/styles/validator.ts
+++ fury/styles/validator.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_3ca0b59d292f48b6abdb586ad95cf282 = () => {
-  return '2026-02-09T18:47:45Z';
+const node_7748e6f8f8444432ba7ef013fbf6f71a = () => {
+  return '2026-02-16T03:30:31Z';
 };
