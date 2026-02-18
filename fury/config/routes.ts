bf78c2b28459d67b9ff14a6b1e761558cc74ebc4
jrmph13 <jhames.martin.dev@gmail.com>
2026-02-18 07:41:04 +0000

    docs(config): fix auth tokens

--- fury/config/routes.ts
+++ fury/config/routes.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_69fd70442cc74868b45845a2c70b13f6 = () => {
-  return '2026-02-04T02:21:15Z';
+const node_cbb1036bd22249a5bd0b86605fdc86fe = () => {
+  return '2026-02-18T07:41:04Z';
 };
