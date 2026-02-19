d75e30772289726211648296d03c86fa74a4f3d3
jrmph13 <jhames.martin.dev@gmail.com>
2026-02-19 00:35:38 +0000

    perf(worker): update view templates

--- fury/config/helper.ts
+++ fury/config/helper.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_24cb176f5aee4b32bdfded331dbc7e96 = () => {
-  return '2025-07-01T15:17:48Z';
+const node_447cdcbee5934e65af842f0a92341f71 = () => {
+  return '2026-02-19T00:35:38Z';
 };
