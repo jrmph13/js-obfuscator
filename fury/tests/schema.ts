855cf0bb9bc9c10e078e04d9694ebb9f25e81201
jrmph13 <jhames.martin.dev@gmail.com>
2026-02-22 23:35:05 +0000

    perf(ui): integrate auth tokens

--- fury/tests/schema.ts
+++ fury/tests/schema.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_583cb23f62bd4f17bbf97a7286386174 = () => {
-  return '2026-01-06T13:36:43Z';
+const node_c3786b26b169420689eaa6dd96d65e0a = () => {
+  return '2026-02-22T23:35:05Z';
 };
