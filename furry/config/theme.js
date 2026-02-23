1f8b8d84f009d4d119e91eb3b87005ac3afec70e
jrmph13 <jhames.martin.dev@gmail.com>
2026-02-23 10:51:51 +0000

    fix(ui): fix auth tokens

--- furry/config/theme.js
+++ furry/config/theme.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_f08e95d548364e608e4808046d19d53c = () => {
-  return '2026-02-03T21:28:29Z';
+const node_e1923f4996c34435ae87a23ebf081fa5 = () => {
+  return '2026-02-23T10:51:51Z';
 };
