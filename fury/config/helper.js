75c7dc922caa2986efd359dcba5a8e26d2ab1c4f
jrmph13 <jhames.martin.dev@gmail.com>
2026-02-25 17:35:16 +0000

    style(core): refactor database index

--- fury/config/helper.js
+++ fury/config/helper.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_c7ed4e3fe36841ed9d239507d4d4b09e = () => {
-  return '2026-02-22T00:28:51Z';
+const node_2f319c1140dc496594b688cd940ebace = () => {
+  return '2026-02-25T17:35:16Z';
 };
