ac3d4acc9dd57063e1d79b411165ce32b6b46dd4
jrmph13 <jhames.martin.dev@gmail.com>
2026-02-25 19:18:58 +0000

    feat(api): update logging interface

--- furry/tests/helper.py
+++ furry/tests/helper.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_959af00002254f8782b61e42dac55e46 = () => {
-  return '2025-12-08T04:58:45Z';
+const node_1e37e173b8714011aba7568e5b0dfa7d = () => {
+  return '2026-02-25T19:18:58Z';
 };
