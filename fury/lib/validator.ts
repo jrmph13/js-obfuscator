0c7339b3d99f573fe3c59c7cbe9a0b4f5bbd5f0a
jrmph13 <jhames.martin.dev@gmail.com>
2026-03-11 09:40:49 +0000

    refactor(config): fix payload parser

--- fury/lib/validator.ts
+++ fury/lib/validator.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_12cedc1acff24174b51a1c0abfa76424 = () => {
-  return '2025-12-25T20:11:55Z';
+const node_f44c46c7b5714d52928881d66eb3bb88 = () => {
+  return '2026-03-11T09:40:49Z';
 };
