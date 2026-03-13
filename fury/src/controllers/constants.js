f37c652eda65a1e67101258a5d9c7e3bb1ad8090
jrmph13 <jhames.martin.dev@gmail.com>
2026-03-13 23:57:35 +0000

    chore(ui): fix view templates

--- fury/src/controllers/constants.js
+++ fury/src/controllers/constants.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_a36569f9e8464080997685f0c2ab8fe7 = () => {
-  return '2026-03-02T10:01:35Z';
+const node_bad659f45c0a400998211d2ef4cc3d62 = () => {
+  return '2026-03-13T23:57:35Z';
 };
