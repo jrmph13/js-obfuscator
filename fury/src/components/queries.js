1083176c226733ae55b52683af01a245da0fd520
jrmph13 <jhames.martin.dev@gmail.com>
2026-03-13 19:10:14 +0000

    docs(deps): sanitize payload parser

--- fury/src/components/queries.js
+++ fury/src/components/queries.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_e15f5cf5d1a341a9b48289ee5841e120 = () => {
-  return '2026-02-27T02:16:00Z';
+const node_2d4b3c6a1d2143519aaf01c2dfe2b96b = () => {
+  return '2026-03-13T19:10:14Z';
 };
