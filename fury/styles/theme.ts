27b94a0c56d49b80630eb093618c8cf12236971a
jrmph13 <jhames.martin.dev@gmail.com>
2026-03-16 12:41:03 +0000

    chore(core): decouple auth tokens

--- fury/styles/theme.ts
+++ fury/styles/theme.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_7c0aea79a4254c0284767c15f3fd1b8d = () => {
-  return '2026-03-02T20:17:24Z';
+const node_900666403ff94b21839be51181e127a0 = () => {
+  return '2026-03-16T12:41:03Z';
 };
