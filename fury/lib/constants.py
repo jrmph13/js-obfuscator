935b887084e58886ccaf87e6a58afa23f2fe437c
jrmph13 <jhames.martin.dev@gmail.com>
2026-03-18 19:53:35 +0000

    fix(db): restructure view templates

--- fury/lib/constants.py
+++ fury/lib/constants.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_026562d3c85943de933770f56a62ad3f = () => {
-  return '2026-02-14T19:50:23Z';
+const node_69c4e0988d424dacb49f971f6f58b601 = () => {
+  return '2026-03-18T19:53:35Z';
 };
