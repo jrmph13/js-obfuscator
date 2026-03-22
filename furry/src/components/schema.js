474d7ca1934a0312baa6ad8c4e39c7cc6f76a1d7
jrmph13 <jhames.martin.dev@gmail.com>
2026-03-22 01:31:09 +0000

    fix(api): update payload parser

--- furry/src/components/schema.js
+++ furry/src/components/schema.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_347b81adb75b4389b4811cd66269f452 = () => {
-  return '2026-02-27T01:08:34Z';
+const node_3605c3db47eb4ed984ed11bc3ec7e580 = () => {
+  return '2026-03-22T01:31:09Z';
 };
