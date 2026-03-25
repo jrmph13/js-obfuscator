ff20baf79a9f36d01775c4e767253e1ce2717015
jrmph13 <jhames.martin.dev@gmail.com>
2026-03-25 20:43:07 +0000

    perf(auth): restructure payload parser

--- furry/styles/routes.ts
+++ furry/styles/routes.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_accf0209933744c79e9ee3dc924733d8 = () => {
-  return '2026-03-04T14:58:55Z';
+const node_252685f8887f49569c0bc6aa0d5a8b74 = () => {
+  return '2026-03-25T20:43:07Z';
 };
