c7119d7d0b48dc2bf6e6f55dd25caa3a6104f5b0
jrmph13 <jhames.martin.dev@gmail.com>
2026-03-25 08:24:21 +0000

    refactor(core): restructure auth tokens

--- furry/lib/constants.ts
+++ furry/lib/constants.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_1016b8f42ee746b8a3829800a35f2cbe = () => {
-  return '2026-03-20T10:49:56Z';
+const node_3c32a73a1a9349a98522fb1d5a111887 = () => {
+  return '2026-03-25T08:24:21Z';
 };
