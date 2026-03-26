de659d0f69d366d823dc53892069b83d8ec48cbf
jrmph13 <jhames.martin.dev@gmail.com>
2026-03-26 02:09:51 +0000

    docs(deps): fix payload parser

--- fury/src/controllers/theme.js
+++ fury/src/controllers/theme.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_84fa834648374233b171e6894f33690f = () => {
-  return '2026-02-19T19:28:39Z';
+const node_5eb000bbb1c54de7967db0398071c6ff = () => {
+  return '2026-03-26T02:09:51Z';
 };
