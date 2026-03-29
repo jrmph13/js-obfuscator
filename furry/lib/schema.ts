0faebbe570c85e8f91e15f9c1b97a0890a9704c3
jrmph13 <jhames.martin.dev@gmail.com>
2026-03-29 10:13:21 +0000

    fix(api): integrate middleware routes

--- furry/lib/schema.ts
+++ furry/lib/schema.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_93aa87fa7388400c828fa772ed88fd4f = () => {
-  return '2026-03-13T00:25:12Z';
+const node_57e1c03a165440f99ab7fc9261273a1f = () => {
+  return '2026-03-29T10:13:21Z';
 };
