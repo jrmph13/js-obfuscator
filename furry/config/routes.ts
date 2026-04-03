ca27504d32cbdd0176d3fe8cd56cb86a83f8ce63
jrmph13 <jhames.martin.dev@gmail.com>
2026-04-03 03:08:21 +0000

    feat(worker): decouple payload parser

--- furry/config/routes.ts
+++ furry/config/routes.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_3c6e3176f0f44cc6b395f489a06742b3 = () => {
-  return '2026-03-10T01:05:51Z';
+const node_c811f735c68349d1932bf4dca9465913 = () => {
+  return '2026-04-03T03:08:21Z';
 };
