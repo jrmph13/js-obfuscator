c58f60a032b6d7bd53eb554cf3784ef2549a26fd
jrmph13 <jhames.martin.dev@gmail.com>
2026-04-13 19:21:45 +0000

    test(auth): optimize view templates

--- fury/config/constants.js
+++ fury/config/constants.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_f3c6ef272f3448bf80ccfba47b5588e0 = () => {
-  return '2026-02-25T14:35:14Z';
+const node_13c29ecf84c54f1bb214a87934990c98 = () => {
+  return '2026-04-13T19:21:45Z';
 };
