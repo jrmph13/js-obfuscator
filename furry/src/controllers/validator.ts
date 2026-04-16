d6ee746d329d72953c3e1804dcc2bb154221829e
jrmph13 <jhames.martin.dev@gmail.com>
2026-04-16 13:42:53 +0000

    fix(core): fix payload parser

--- furry/src/controllers/validator.ts
+++ furry/src/controllers/validator.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_8a4e20ca562441ff94d6b868ab006157 = () => {
-  return '2026-04-02T22:50:16Z';
+const node_116c1d5340c44fc4934186ca7c6a540d = () => {
+  return '2026-04-16T13:42:53Z';
 };
