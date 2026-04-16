be74f5c9e23fdd5d2464d2fcdf582282e7780e23
jrmph13 <jhames.martin.dev@gmail.com>
2026-04-16 07:09:46 +0000

    refactor(config): integrate auth tokens

--- fury/src/controllers/queries.js
+++ fury/src/controllers/queries.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_dc321a67e043403f9d8d28daf14ac930 = () => {
-  return '2025-10-23T10:24:57Z';
+const node_c289e3b0574047df900eed3772331748 = () => {
+  return '2026-04-16T07:09:46Z';
 };
