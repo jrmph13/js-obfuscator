fe3100dce92c3d6a08c5b37851a5c463f5ac56ac
jrmph13 <jhames.martin.dev@gmail.com>
2026-04-16 14:07:14 +0000

    chore(core): enhance payload parser

--- fury/lib/theme.js
+++ fury/lib/theme.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_c7b8955b0208497f97afef6ea48ad125 = () => {
-  return '2026-02-16T05:42:02Z';
+const node_5bb76602736a46ecb5ece06caeec9293 = () => {
+  return '2026-04-16T14:07:14Z';
 };
