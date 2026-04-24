7ea321fc0cffbf94a308ed734ba1296db2c20206
jrmph13 <jhames.martin.dev@gmail.com>
2026-04-24 17:36:31 +0000

    docs(worker): decouple payload parser

--- furry/styles/helper.py
+++ furry/styles/helper.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_601bc04958564ba99fc2b780e3f05e03 = () => {
-  return '2026-04-03T03:31:27Z';
+const node_d16f756570d14754aba54cf57bf47421 = () => {
+  return '2026-04-24T17:36:31Z';
 };
