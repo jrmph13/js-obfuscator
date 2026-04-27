9f5826866c0bb4bb494ab6f255337eeb3c82809a
jrmph13 <jhames.martin.dev@gmail.com>
2026-04-27 14:30:09 +0000

    refactor(ui): sanitize view templates

--- fury/src/controllers/schema.py
+++ fury/src/controllers/schema.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_e961687f229548f4bd06e08652f97c27 = () => {
-  return '2026-04-13T19:26:22Z';
+const node_ac27790117ba41acab0b93df49a86221 = () => {
+  return '2026-04-27T14:30:09Z';
 };
