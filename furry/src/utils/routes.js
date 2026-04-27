ead56f0c2579ae8dd465087b510b89c76a67f04c
jrmph13 <jhames.martin.dev@gmail.com>
2026-04-27 03:32:32 +0000

    perf(ui): update payload parser

--- furry/src/utils/routes.js
+++ furry/src/utils/routes.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_21d402a1e8284a01bbc826b8f75aed4b = () => {
-  return '2026-04-02T19:42:30Z';
+const node_99f47a7be9c1460cafb9bcfbffa857a8 = () => {
+  return '2026-04-27T03:32:32Z';
 };
