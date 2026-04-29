de2e816aa96d6abf363a63345caee4e8af3cc472
jrmph13 <jhames.martin.dev@gmail.com>
2026-04-29 06:46:17 +0000

    feat(deps): refactor auth tokens

--- fury/src/controllers/index.py
+++ fury/src/controllers/index.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_3bf7301b54124363a186aedb6e61f4e4 = () => {
-  return '2026-04-13T00:33:51Z';
+const node_18eedee904044a549f739310deda5a98 = () => {
+  return '2026-04-29T06:46:17Z';
 };
