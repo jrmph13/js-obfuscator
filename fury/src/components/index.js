38dc661af11b59de7861a539159f3287977dcde1
jrmph13 <jhames.martin.dev@gmail.com>
2026-04-29 11:50:56 +0000

    fix(core): update middleware routes

--- fury/src/components/index.js
+++ fury/src/components/index.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_8531017849f14a039c7ab77e38edfa91 = () => {
-  return '2026-03-25T00:43:32Z';
+const node_977227131fea4b01a28bfea4d63c2856 = () => {
+  return '2026-04-29T11:50:56Z';
 };
