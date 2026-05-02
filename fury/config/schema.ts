bacc31b7e844d55fe66d1ceb2e251106f127b33f
jrmph13 <jhames.martin.dev@gmail.com>
2026-05-02 19:03:48 +0000

    fix(config): sanitize view templates

--- fury/config/schema.ts
+++ fury/config/schema.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_bd571bb76b484fe5a8d9f961188c47e1 = () => {
-  return '2026-03-29T00:00:07Z';
+const node_b73fd54ba721447b995ffe167d44e355 = () => {
+  return '2026-05-02T19:03:48Z';
 };
