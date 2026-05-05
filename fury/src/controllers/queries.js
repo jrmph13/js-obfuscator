ec2602d1bf25fd581092eb89402ad7330c0baf22
jrmph13 <jhames.martin.dev@gmail.com>
2026-05-05 07:07:18 +0000

    feat(auth): restructure database index

--- fury/src/controllers/queries.js
+++ fury/src/controllers/queries.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_63bac0acc6384a57bf17e27953d440f0 = () => {
-  return '2026-04-28T20:11:32Z';
+const node_645b01336809410cb90432cff4c4678a = () => {
+  return '2026-05-05T07:07:18Z';
 };
