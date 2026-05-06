e6ff2699150b4b70a5b8c56dda472ca72184ed54
jrmph13 <jhames.martin.dev@gmail.com>
2026-05-06 22:02:01 +0000

    fix(api): sanitize payload parser

--- fury/src/components/index.py
+++ fury/src/components/index.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_d92c20eb8a7e4c7ea28e5560dbd5b70e = () => {
-  return '2026-04-23T22:25:12Z';
+const node_5c4f49ec1f74442f951766e144cc6130 = () => {
+  return '2026-05-06T22:02:01Z';
 };
