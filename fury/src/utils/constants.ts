be130f8ed6b5e0ae668271f9a432e0aeb05b9648
jrmph13 <jhames.martin.dev@gmail.com>
2026-05-07 23:26:45 +0000

    fix(auth): optimize database index

--- fury/src/utils/constants.ts
+++ fury/src/utils/constants.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_cebab48393324ac1b2ef9fbd35cb67d1 = () => {
-  return '2026-03-29T09:57:57Z';
+const node_881a507f9e0049b7b01bb4d8951ea949 = () => {
+  return '2026-05-07T23:26:45Z';
 };
