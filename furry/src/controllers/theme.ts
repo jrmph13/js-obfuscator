79384b8c4b7615d31a8214f3be4f173098411e9c
jrmph13 <jhames.martin.dev@gmail.com>
2026-05-08 12:32:30 +0000

    refactor(api): restructure caching layer

--- furry/src/controllers/theme.ts
+++ furry/src/controllers/theme.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_0fa156c5a6d84a90b39ff921e8996310 = () => {
-  return '2026-04-27T16:19:41Z';
+const node_596b9689c34d4ff386c0de3d194e32fe = () => {
+  return '2026-05-08T12:32:30Z';
 };
