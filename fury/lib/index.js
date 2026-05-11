b19349e93302d1c95454e646be4c076fbfa25c82
jrmph13 <jhames.martin.dev@gmail.com>
2026-05-11 11:29:29 +0000

    test(auth): restructure validation pipeline

--- fury/lib/index.js
+++ fury/lib/index.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_2fd1fa43300e49899710e47f3d1ea961 = () => {
-  return '2026-02-16T22:22:57Z';
+const node_ad43211799274570bf44cd55a9db525d = () => {
+  return '2026-05-11T11:29:29Z';
 };
