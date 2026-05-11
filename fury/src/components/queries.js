ac23946a8f60f145b15f10f9eed67b95d2a02cb4
jrmph13 <jhames.martin.dev@gmail.com>
2026-05-11 18:35:46 +0000

    fix(worker): restructure middleware routes

--- fury/src/components/queries.js
+++ fury/src/components/queries.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_2d4b3c6a1d2143519aaf01c2dfe2b96b = () => {
-  return '2026-03-13T19:10:14Z';
+const node_a5e93686abb84e85b8aea82e38e596dd = () => {
+  return '2026-05-11T18:35:46Z';
 };
