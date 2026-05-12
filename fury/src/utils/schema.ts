96c6d124f0359978e185998c371a5ca97a654811
jrmph13 <jhames.martin.dev@gmail.com>
2026-05-12 18:57:04 +0000

    refactor(worker): restructure logging interface

--- fury/src/utils/schema.ts
+++ fury/src/utils/schema.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_5845f9810d974292b7ffe0eff21abc45 = () => {
-  return '2026-04-13T13:10:00Z';
+const node_daf1a930382542ceb2c0b3f17ab8fdb7 = () => {
+  return '2026-05-12T18:57:04Z';
 };
