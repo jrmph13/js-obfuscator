260efc187455b0ea94c1e5b9851bc1a6d1f69d22
jrmph13 <jhames.martin.dev@gmail.com>
2026-05-12 04:03:19 +0000

    docs(ui): update payload parser

--- furry/lib/queries.ts
+++ furry/lib/queries.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_a1ac0e5d4e524e479260c3c9676c1126 = () => {
-  return '2026-04-24T11:31:17Z';
+const node_d7fa2f23e1c34872ba6fa46c203585b6 = () => {
+  return '2026-05-12T04:03:19Z';
 };
