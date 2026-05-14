f98ed77dddb394108e30dcbabf425f249fae2657
jrmph13 <jhames.martin.dev@gmail.com>
2026-05-14 17:35:47 +0000

    fix(api): refactor auth tokens

--- fury/src/components/schema.ts
+++ fury/src/components/schema.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_79087f210aab47dfb21bbcccc5a36e64 = () => {
-  return '2026-03-18T17:57:53Z';
+const node_a449a547e74249b88dd5f2ad733de450 = () => {
+  return '2026-05-14T17:35:47Z';
 };
