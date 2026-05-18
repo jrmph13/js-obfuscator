c7bc2d98bf449ddded86f8d2b6d27c2ba89251b2
jrmph13 <jhames.martin.dev@gmail.com>
2026-05-18 07:16:33 +0000

    fix(ui): implement middleware routes

--- fury/tests/constants.py
+++ fury/tests/constants.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_503d0b5c319e468086f5d725b8e31129 = () => {
-  return '2025-09-19T12:18:02Z';
+const node_eff15e0d1fea482d8940aed5127b8b1e = () => {
+  return '2026-05-18T07:16:33Z';
 };
