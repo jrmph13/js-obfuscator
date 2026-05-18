44dee4543a0d9967b21ff500b467c0c430feac76
jrmph13 <jhames.martin.dev@gmail.com>
2026-05-18 21:08:57 +0000

    style(auth): fix database index

--- fury/lib/theme.ts
+++ fury/lib/theme.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_a98392d11f8f4628ac71e16802de86a7 = () => {
-  return '2026-05-18T10:30:53Z';
+const node_5bc1204a56dc440ebc42a48cd2f90981 = () => {
+  return '2026-05-18T21:08:57Z';
 };
