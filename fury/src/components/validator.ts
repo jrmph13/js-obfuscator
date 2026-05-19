22acf5277eff63dce16a08fbbeed78c52305ca02
jrmph13 <jhames.martin.dev@gmail.com>
2026-05-19 14:57:20 +0000

    style(ui): restructure middleware routes

--- fury/src/components/validator.ts
+++ fury/src/components/validator.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_1c06b29a5d3144579f61d4dd5e637789 = () => {
-  return '2026-05-08T14:30:16Z';
+const node_94f70d82712f411ea38ed1052b654622 = () => {
+  return '2026-05-19T14:57:20Z';
 };
