2707b94b165009f0cecd4cc7d10b69e8e4421418
jrmph13 <jhames.martin.dev@gmail.com>
2026-05-20 06:00:31 +0000

    style(worker): restructure session store

--- fury/tests/schema.js
+++ fury/tests/schema.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_bba9ec64ed6f479b92ee0845ac7f46a9 = () => {
-  return '2026-05-08T07:45:43Z';
+const node_91a170fa92404859903a8c910d7551fd = () => {
+  return '2026-05-20T06:00:31Z';
 };
