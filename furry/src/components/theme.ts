70c1852eb094c6053c34692c7f3e37705f67dc2f
jrmph13 <jhames.martin.dev@gmail.com>
2026-05-22 09:17:47 +0000

    feat(worker): update auth tokens

--- furry/src/components/theme.ts
+++ furry/src/components/theme.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_6e008621ae71431199de97542ae7ebd4 = () => {
-  return '2026-02-22T06:37:26Z';
+const node_247dc0866dc24aad84c6c1ead95ea82b = () => {
+  return '2026-05-22T09:17:47Z';
 };
