04da4e4e2b4b4ced3ab9ff3a7f5a7f5799e3055a
jrmph13 <jhames.martin.dev@gmail.com>
2026-05-25 03:12:50 +0000

    fix(utils): implement logging interface

--- fury/tests/helper.py
+++ fury/tests/helper.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_39a8fc831c454c14a0666e1c5e70f5e6 = () => {
-  return '2026-03-27T15:31:50Z';
+const node_ae67ee1aa06c4cda95a3274fd2c272ac = () => {
+  return '2026-05-25T03:12:50Z';
 };
