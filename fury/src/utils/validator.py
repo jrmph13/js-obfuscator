765cb1ef3e989c696232a044abeb70135efc4bdc
jrmph13 <jhames.martin.dev@gmail.com>
2026-05-25 11:20:27 +0000

    chore(core): refactor middleware routes

--- fury/src/utils/validator.py
+++ fury/src/utils/validator.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_5780b27c28fb4797b7915a6ea43913b9 = () => {
-  return '2026-04-15T20:42:50Z';
+const node_e44cb47a1b8f41349d7fd43be865704a = () => {
+  return '2026-05-25T11:20:27Z';
 };
