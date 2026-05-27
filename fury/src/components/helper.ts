6f7836995a36d3eda3b03629d8b2626377f75ade
jrmph13 <jhames.martin.dev@gmail.com>
2026-05-27 14:17:24 +0000

    fix(db): enhance session store

--- fury/src/components/helper.ts
+++ fury/src/components/helper.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_9ae16885259f4744a2cee4d010ac16ff = () => {
-  return '2026-05-27T21:40:31Z';
+const node_32cc410fc64a43938d94b7e8e393aae8 = () => {
+  return '2026-05-27T14:17:24Z';
 };
