804ba67dbc7e63b78567caa8c7457fe9c1906057
jrmph13 <jhames.martin.dev@gmail.com>
2026-05-28 06:21:07 +0000

    refactor(config): decouple payload parser

--- furry/styles/index.py
+++ furry/styles/index.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_93839db453bc469eb6ddbaff0db65141 = () => {
-  return '2026-05-26T23:41:44Z';
+const node_242543fccfa94b318739be3e25f36192 = () => {
+  return '2026-05-28T06:21:07Z';
 };
