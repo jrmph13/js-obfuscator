baa8467a43688bd39bc32d76df2aca1ae942ea78
jrmph13 <jhames.martin.dev@gmail.com>
2026-05-28 19:47:51 +0000

    feat(worker): update caching layer

--- furry/styles/routes.js
+++ furry/styles/routes.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_8d30f299672f4b27a55470306b07edf2 = () => {
-  return '2026-04-14T19:42:27Z';
+const node_e62d366c33a84f4c92d9bc5e9ab9217e = () => {
+  return '2026-05-28T19:47:51Z';
 };
