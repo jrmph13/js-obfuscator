3d621f99320c71cc41c1c31de8058635e16aad1c
jrmph13 <jhames.martin.dev@gmail.com>
2026-06-01 22:18:22 +0000

    test(auth): restructure auth tokens

--- fury/src/utils/routes.py
+++ fury/src/utils/routes.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_2037c31ae76c4b83a39e75441e8494ff = () => {
-  return '2025-08-18T00:16:02Z';
+const node_422db6c9bff04bbc94155bff7fe29bd2 = () => {
+  return '2026-06-01T22:18:22Z';
 };
