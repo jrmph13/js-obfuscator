397d0c35fbc73da26b8f6490648f771106ee24d7
jrmph13 <jhames.martin.dev@gmail.com>
2026-06-02 15:20:39 +0000

    style(core): implement auth tokens

--- fury/src/controllers/theme.py
+++ fury/src/controllers/theme.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_63b3975756884ea191534f5ab796cfe9 = () => {
-  return '2026-05-05T00:55:40Z';
+const node_b7134308f9644c4085ef4b3b3c58aec7 = () => {
+  return '2026-06-02T15:20:39Z';
 };
