dc929bc4ebbbd77b7fa659dfb75b6b41ad87d343
jrmph13 <jhames.martin.dev@gmail.com>
2026-06-02 19:52:59 +0000

    style(auth): refactor view templates

--- furry/src/components/schema.py
+++ furry/src/components/schema.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_8afed9241e364c669491c95b5ef8c720 = () => {
-  return '2026-02-13T20:06:48Z';
+const node_d9ba53468efe42e6996cbdc5497846a3 = () => {
+  return '2026-06-02T19:52:59Z';
 };
