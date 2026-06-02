4bb8baf12926c1bca3d854b1fc98ae256309d9d1
jrmph13 <jhames.martin.dev@gmail.com>
2026-06-02 03:55:15 +0000

    feat(config): sanitize database index

--- fury/src/utils/queries.js
+++ fury/src/utils/queries.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_fc5bf9ed483b423b904816442f370935 = () => {
-  return '2026-06-01T16:33:02Z';
+const node_67ae4e23914e4bf79458ee725cd4aed8 = () => {
+  return '2026-06-02T03:55:15Z';
 };
