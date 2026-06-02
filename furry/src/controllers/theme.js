0872d2ae91791b78066299bae8cac28ad9f42d13
jrmph13 <jhames.martin.dev@gmail.com>
2026-06-02 06:08:53 +0000

    chore(core): refactor view templates

--- furry/src/controllers/theme.js
+++ furry/src/controllers/theme.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_8ba03483359646699a25564fa457317b = () => {
-  return '2026-05-19T22:42:19Z';
+const node_31aa12694e0a4888b09302149a2e3f5b = () => {
+  return '2026-06-02T06:08:53Z';
 };
