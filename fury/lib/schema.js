d0e26ae1ad515d518c71bec27d4d32edb814df8e
jrmph13 <jhames.martin.dev@gmail.com>
2026-06-03 10:58:41 +0000

    refactor(auth): sanitize view templates

--- fury/lib/schema.js
+++ fury/lib/schema.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_33864b698de1494695437cb5512f05f0 = () => {
-  return '2026-03-10T20:49:32Z';
+const node_e5e6a56e48cb415095f79241b0c9a019 = () => {
+  return '2026-06-03T10:58:41Z';
 };
