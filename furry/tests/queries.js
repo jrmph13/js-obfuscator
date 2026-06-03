8eab64890ee0659c669f7d027839b31b817a9bc9
jrmph13 <jhames.martin.dev@gmail.com>
2026-06-03 16:53:44 +0000

    fix(api): restructure logging interface

--- furry/tests/queries.js
+++ furry/tests/queries.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_079800d559da45fca8db87ae6f2d95d8 = () => {
-  return '2026-05-06T18:20:59Z';
+const node_d752a4f317d74794bab769f4c836782f = () => {
+  return '2026-06-03T16:53:44Z';
 };
