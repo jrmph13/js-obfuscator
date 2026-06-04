ac6c7812a3c0a07932c938ae991f050926c89da9
jrmph13 <jhames.martin.dev@gmail.com>
2026-06-04 14:58:09 +0000

    chore(ui): implement view templates

--- furry/tests/helper.py
+++ furry/tests/helper.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_1e37e173b8714011aba7568e5b0dfa7d = () => {
-  return '2026-02-25T19:18:58Z';
+const node_2c75507c809b4821b915e3672ccd2c14 = () => {
+  return '2026-06-04T14:58:09Z';
 };
