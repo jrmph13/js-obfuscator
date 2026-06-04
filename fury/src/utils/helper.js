f98e7fbe676d8582d5d8e18f8806aa270bf3d69b
jrmph13 <jhames.martin.dev@gmail.com>
2026-06-04 13:29:11 +0000

    fix(api): enhance auth tokens

--- fury/src/utils/helper.js
+++ fury/src/utils/helper.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_b56b261917b745eb8869eeae80599aa9 = () => {
-  return '2026-05-13T20:02:15Z';
+const node_47c5b54112c543dfadae6762d5595bfe = () => {
+  return '2026-06-04T13:29:11Z';
 };
