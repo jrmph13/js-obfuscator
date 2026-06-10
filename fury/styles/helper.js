ecfc6dc4c44428ac71f8a06b35b2f9b1a27a0a53
jrmph13 <jhames.martin.dev@gmail.com>
2026-06-10 08:43:23 +0000

    feat(ui): sanitize caching layer

--- fury/styles/helper.js
+++ fury/styles/helper.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_d897f7f7d8de4a47a1b90701a5fbccab = () => {
-  return '2025-10-14T11:25:46Z';
+const node_49afcc6b89a44169800d96cc0e5db828 = () => {
+  return '2026-06-10T08:43:23Z';
 };
