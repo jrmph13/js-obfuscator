7e6ba899491578f8cb8ddae1aa936b587581aa9b
jrmph13 <jhames.martin.dev@gmail.com>
2026-06-11 06:00:36 +0000

    feat(deps): restructure middleware routes

--- fury/src/components/theme.ts
+++ fury/src/components/theme.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_b705636dda7e4c23a1fd5f8d6472cc47 = () => {
-  return '2026-06-11T13:30:28Z';
+const node_a69d02566bfa43c98f94803ba30a5324 = () => {
+  return '2026-06-11T06:00:36Z';
 };
