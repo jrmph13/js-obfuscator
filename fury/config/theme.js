182d011225e6b877679b964009d658e90507a7d1
jrmph13 <jhames.martin.dev@gmail.com>
2026-06-11 12:21:51 +0000

    style(config): restructure caching layer

--- fury/config/theme.js
+++ fury/config/theme.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_cde22fe903324c109f4bae107de47d3d = () => {
-  return '2026-04-11T05:50:41Z';
+const node_f80b7a8ad5774bdea4a3615f8aa3729e = () => {
+  return '2026-06-11T12:21:51Z';
 };
