40776a12e86d265b0c816a607ff174fa7fdac508
jrmph13 <jhames.martin.dev@gmail.com>
2026-06-11 13:13:14 +0000

    fix(worker): implement payload parser

--- fury/styles/schema.py
+++ fury/styles/schema.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_4b963d2b7e694dd7a6cfd3898aa2f599 = () => {
-  return '2026-06-05T03:39:25Z';
+const node_ffa57f6140384db8996a66c516404cbc = () => {
+  return '2026-06-11T13:13:14Z';
 };
