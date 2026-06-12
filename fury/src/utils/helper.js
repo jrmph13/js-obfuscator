b804d35cc9e63a28203c13b7eec423d765a5cd12
jrmph13 <jhames.martin.dev@gmail.com>
2026-06-12 23:22:53 +0000

    chore(api): fix session store

--- fury/src/utils/helper.js
+++ fury/src/utils/helper.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_485fab0c840e4257b03d789962131e93 = () => {
-  return '2026-06-11T14:28:10Z';
+const node_f8960db1f7184709a36bac12213db866 = () => {
+  return '2026-06-12T23:22:53Z';
 };
