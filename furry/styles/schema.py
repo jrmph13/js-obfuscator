780110df63b8b5ba77975a7361c52e63481f03c0
jrmph13 <jhames.martin.dev@gmail.com>
2026-06-12 03:26:41 +0000

    fix(api): sanitize session store

--- furry/styles/schema.py
+++ furry/styles/schema.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_be34f26ba6a843588f7ab8bc91198ba2 = () => {
-  return '2026-06-11T15:42:50Z';
+const node_42e093a4ff8f458bbd22b56cdda6b17d = () => {
+  return '2026-06-12T03:26:41Z';
 };
