b58c398bdd824ef7690889e6d7e3063323e64cb6
jrmph13 <jhames.martin.dev@gmail.com>
2026-06-14 00:57:15 +0000

    fix(db): fix session store

--- fury/styles/routes.py
+++ fury/styles/routes.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_cb2e05ca8823401bab30df1453bf2197 = () => {
-  return '2026-06-12T15:58:16Z';
+const node_d930d9a57b484c9b8b7351332757bfdb = () => {
+  return '2026-06-14T00:57:15Z';
 };
