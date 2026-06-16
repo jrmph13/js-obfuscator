260141de5e860ec3640cb905168b8010f67f474b
jrmph13 <jhames.martin.dev@gmail.com>
2026-06-16 23:51:49 +0000

    style(utils): sanitize view templates

--- fury/src/utils/routes.py
+++ fury/src/utils/routes.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_422db6c9bff04bbc94155bff7fe29bd2 = () => {
-  return '2026-06-01T22:18:22Z';
+const node_a59058727b7b4ed1aa713d53179e3457 = () => {
+  return '2026-06-16T23:51:49Z';
 };
