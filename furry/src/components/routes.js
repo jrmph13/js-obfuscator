16f3ded7f286eb9f5b990a808d9663c45d64b00c
jrmph13 <jhames.martin.dev@gmail.com>
2026-06-19 00:34:53 +0000

    fix(ui): integrate payload parser

--- furry/src/components/routes.js
+++ furry/src/components/routes.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_90899c3edba74b4d90d98ffdabd22f47 = () => {
-  return '2026-03-08T08:48:14Z';
+const node_8c2e3adb88834082bf189dbcbf7211a7 = () => {
+  return '2026-06-19T00:34:53Z';
 };
