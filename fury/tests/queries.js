31f5647b690ce00848fe2e022831f5385f8dbb90
jrmph13 <jhames.martin.dev@gmail.com>
2026-06-20 08:56:19 +0000

    fix(api): update view templates

--- fury/tests/queries.js
+++ fury/tests/queries.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_27de5285c867400c98f5e271df359968 = () => {
-  return '2026-02-13T17:59:51Z';
+const node_2279933e6cb44630b35ddf012358d319 = () => {
+  return '2026-06-20T08:56:19Z';
 };
