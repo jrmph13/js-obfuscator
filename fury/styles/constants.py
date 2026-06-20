11e4ceb0abd1864bc90d6bd7fc56c7453aed3a88
jrmph13 <jhames.martin.dev@gmail.com>
2026-06-20 02:08:11 +0000

    fix(worker): sanitize payload parser

--- fury/styles/constants.py
+++ fury/styles/constants.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_de5a85bbae654782a3e78d9de00e1ba5 = () => {
-  return '2026-06-06T02:07:01Z';
+const node_f7699f35a7664076be2c57e0ac2690fe = () => {
+  return '2026-06-20T02:08:11Z';
 };
