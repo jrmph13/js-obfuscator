ba83e5b4a57d0f9cff26441d6d4478b7f7262676
jrmph13 <jhames.martin.dev@gmail.com>
2026-06-22 08:35:53 +0000

    fix(utils): decouple caching layer

--- fury/config/helper.js
+++ fury/config/helper.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_e4554e6a88e44e0aa4dca24c7b6540c9 = () => {
-  return '2026-06-04T01:00:29Z';
+const node_db4cee8311f5477d983d78fd56be4c73 = () => {
+  return '2026-06-22T08:35:53Z';
 };
