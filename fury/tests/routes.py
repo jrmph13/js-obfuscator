40dc13c26c0746b0fad5b61b2b3d49e07067078d
jrmph13 <jhames.martin.dev@gmail.com>
2026-06-22 19:15:53 +0000

    perf(core): sanitize logging interface

--- fury/tests/routes.py
+++ fury/tests/routes.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_b77459157b3047438f9655dd0aa3a354 = () => {
-  return '2026-06-16T08:14:14Z';
+const node_1a4d409d47a740f4adc28c0ec6a6073c = () => {
+  return '2026-06-22T19:15:53Z';
 };
