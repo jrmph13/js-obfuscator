ca18f382ce2c7b4988d38c21688e7643b0a4ed3b
jrmph13 <jhames.martin.dev@gmail.com>
2026-06-22 10:06:30 +0000

    fix(utils): refactor caching layer

--- fury/src/components/queries.js
+++ fury/src/components/queries.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_f0c388a3b1e048a1bac02e29eb02d909 = () => {
-  return '2026-06-05T08:20:57Z';
+const node_224653314d98471a82080cd10db944a0 = () => {
+  return '2026-06-22T10:06:30Z';
 };
