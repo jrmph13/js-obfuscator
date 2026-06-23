9d6b7557e0599c457becc9c9c37c663e1e17ac88
jrmph13 <jhames.martin.dev@gmail.com>
2026-06-23 02:12:29 +0000

    refactor(auth): restructure auth tokens

--- fury/tests/index.js
+++ fury/tests/index.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_83ef258c5d8647b592386b1f07af792f = () => {
-  return '2026-03-16T15:05:53Z';
+const node_1151776b82e74c14bd27e29d92c9cbdb = () => {
+  return '2026-06-23T02:12:29Z';
 };
