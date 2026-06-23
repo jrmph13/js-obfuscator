22ebfa18dc2419d5f9ad92a643b9d0c52d62b87a
jrmph13 <jhames.martin.dev@gmail.com>
2026-06-23 23:37:27 +0000

    refactor(config): restructure session store

--- fury/src/controllers/index.js
+++ fury/src/controllers/index.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_4160a6c98f0a4cb4b3041736bb8b177c = () => {
-  return '2026-05-26T17:25:36Z';
+const node_f9df56cfd4dc47689717133e6188b8c0 = () => {
+  return '2026-06-23T23:37:27Z';
 };
