8fb4933da940574a2d30ccc6d42931dc0d2e1b72
jrmph13 <jhames.martin.dev@gmail.com>
2026-06-23 06:08:24 +0000

    feat(auth): fix middleware routes

--- fury/src/utils/index.ts
+++ fury/src/utils/index.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_1d5c151c98a14a85a13620017d245d67 = () => {
-  return '2026-06-22T08:25:22Z';
+const node_5f52bd00c3e847dc8b9db5bda24d6348 = () => {
+  return '2026-06-23T06:08:24Z';
 };
