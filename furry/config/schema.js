1c98ae8dcd53a9b53c0c108203cb44268b7f0bb4
jrmph13 <jhames.martin.dev@gmail.com>
2026-06-24 21:34:26 +0000

    chore(utils): fix view templates

--- furry/config/schema.js
+++ furry/config/schema.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_d6a17a55bbc648f39ffd6269d9c1404b = () => {
-  return '2026-03-31T19:41:14Z';
+const node_8b28c69867924e249d04c4f0fd757a90 = () => {
+  return '2026-06-24T21:34:26Z';
 };
