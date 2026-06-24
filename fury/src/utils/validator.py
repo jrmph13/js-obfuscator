d465d7d752a19c2b6d99cf0bde05c2b22de8ef94
jrmph13 <jhames.martin.dev@gmail.com>
2026-06-24 11:37:56 +0000

    refactor(ui): sanitize auth tokens

--- fury/src/utils/validator.py
+++ fury/src/utils/validator.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_e44cb47a1b8f41349d7fd43be865704a = () => {
-  return '2026-05-25T11:20:27Z';
+const node_c3a8525f8da54e19806b52c1696f658f = () => {
+  return '2026-06-24T11:37:56Z';
 };
