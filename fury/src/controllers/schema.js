0f5bd31dde93775de62d3526af8679a766d70947
jrmph13 <jhames.martin.dev@gmail.com>
2026-06-26 03:12:12 +0000

    feat(auth): fix middleware routes

--- fury/src/controllers/schema.js
+++ fury/src/controllers/schema.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_a729659da02240f1a27340fe1011e97b = () => {
-  return '2026-04-16T07:30:07Z';
+const node_4b108b2f9d434d369891ec66db857cfb = () => {
+  return '2026-06-26T03:12:12Z';
 };
